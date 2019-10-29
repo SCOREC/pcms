5650d0e997adc9264d4a02d301320cb415dc37e2
Abolaji Adesoji <adesoa@remus.scorec.rpi.edu>
2019-10-28 20:06:47 -0400

    rhel7 config files added

---
+++ drv/build/scorec-rhel7-config.sh
@@ -0,0 +1,23 @@
+#/bin/bash -x
+
+# edit these params
+DEVLOC=users
+DEVDIR=dev
+CC=mpicc
+CXX=mpicxx
+FTN=mpif90
+IMPLICIT_PETSC=OFF
+PREFIX=${PWD}/../../install/drv
+
+cmake .. \
+      -DCMAKE_C_COMPILER=$CC \
+      -DCMAKE_CXX_COMPILER=$CXX \
+      -DCMAKE_Fortran_COMPILER=$FTN \
+      -DSCOREC_DIR=$PUMI_ROOT/lib/cmake/SCOREC/ \
+      -DENABLE_TESTING=ON \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DADIOS_INCLUDE_DIRS="$PWD/../../cpl/cmake/adios" \
+      -DMPI_INCLUDE_PATH="/opt/scorec/spack/install/linux-rhel7-x86_64/gcc-7.3.0/mpich-3.3-diz4f6ieln25ouifyc7ndtqlfksom6nb/include" \
+#      ${CONFIG_PARAMS}
+

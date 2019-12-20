79e88a035cb213f2059c9955892ca0e5c7034719
Abolaji Dami Adesoji <damilare@cori07.nersc.gov>
2019-12-20 07:40:49 -0800

    overriding the library with the updated state

--- cpl/src/wdmapp.cc
+++ cpl/src/wdmapp.cc
@@ -1,91 +1,81 @@
 //#include <mpi.h>
-#include<stdio.h>
 #include"cpl_init.h"
-#include<iostream>
 #include<sstream> //for the stream run function call
-//#include<cstdlib>
-//#include<cstring>
 #include<unistd.h>
 #include<sys/wait.h>
 #include<sys/stat.h>
-#include<fstream>
 #include<string>
-//#include<string.h>
 
-// TASK_1: create a new process to run the binary serially
-// TASK_2: Using MPI, duplicate the number of process - currently run as mpirun -np executable called within benchmark
-// TASK_3: Automate the reading of the number of processes needed
-// TASK_4: Check the run scripts for the existing coupling and draw up something similar in cpp
-// Task_5: Adopt the process for both GENE & XGC
+// TASK_1: Execute both codes using the system commands 
+// TASK_2: validate OOP implementation is necessary i.e. considering process level coordination
+// TASK_3: Compare both codes
 
-struct run_coupling  
+struct couple  
 {
   private :
-    const std::string XGC_lib/*, GENE_lib*/;
-    struct stat buffer;
+    const std::string XGC_lib;
+    const int N_X, n_X;
+    struct stat buf;
 
   public: 
-    run_coupling(const std::string& X_lib /*, const std::string& G_lib*/)
-      : XGC_lib(X_lib)/*, GENE_lib(G_lib) */ {}
-    //gain access to the binaries via this functions
+    couple(const std::string& X_lib, const int& nodes_X, const int& threads_X )
+      : XGC_lib(X_lib), N_X(nodes_X), n_X(threads_X) {}
+
+    //access binaries 
     const std::string& getNameX() const { return XGC_lib;}  
-    // const std::string& getNameG() const { return GENE_lib;}  
 
     // check that the XGC library exists
-    inline bool check_lib(const std::string& lib){
-      return(stat (lib.c_str(), &buffer) == 0);
+    inline bool check_libs(){ // if checking const char* file-> stat(file,&buf)
+      return(stat (XGC_lib.c_str(), &buf) == 0);// use "continue" when checking for GENE
     }
     
-    // MPI run is exactly as the standard run, calling MPIRUN
-    //this was to create a single process to run the binary
-    int ls(const char *dir){
-      int pid, status;
-      if (pid=fork()){
-        waitpid(pid, &status, 0);
-      } else {
-        const char executable[] = "bin/ls";
-        execl(executable, executable, dir, NULL);
-      }
-      return status;
-    }
-
-
-    int run_XGC(const char *dir){
-      int pid, status;
-      if (pid=fork()){
-        waitpid(pid, &status, 0);
-      } else {
-        const char executable[] = "bin/ls";
-        execl(executable, executable, dir, NULL);
-      }
-      return status;
-    }
-
-
-    int string_run()
+    // standard srun
+    int run_X()
     {
       std::stringstream stream;
-      stream<< XGC_lib
-        <<" "
-        <<"argument";
+      stream << "srun -N " << N_X  <<" -n " << n_X <<" "  << XGC_lib <<" > XGC.output 2> XGC.error";
  	return system(stream.str().c_str());
     }
+    
+    //this creates a single process to run the binary
+    int run_X_2(const char *dir){
+      int pid, status;
+      if (pid=fork()){
+        waitpid(pid, &status, 0);
+      } else {
+        const char* executable = XGC_lib.c_str();
+        std::string str = "srun -N " + N_X;//+ "-n " + n_X + " " +  XGC_lib + " > XGC.output 2> XGC.error";
+        execl(executable, str.c_str(), (char*)0);
+      }
+      return status;
+    }
+
 };
 
 int main(int argc, char **argv){
 //	MPI_Init(NULL, NULL);
- //Ignore the use of OOP for this first iteration
-  std::string MPI_RUN = "mpirun ";
-  std::string PROC = "64 ";
-  std::string XPATH = "../../WDM_XGC/xgc-es-coupling";
-  std::string str = MPI_RUN + "-np " + PROC + XPATH;
+
+  std::string SRUN = "srun ", PROC_X = "16", PROC_G = "16", THREAD_X = "512", THREAD_G="384";
+  // full proc_x => 256 & thread_x => 6144
+  
+  std::string X_PATH = "~/XGC_adios2/xgc-es", X_OPT = " > XGC.output 2> XGC.error";
+  std::string G_PATH = "~/gene_Adios2_cori/bin/gene_cori", G_OPT = " > GENE.output 2> GENE.error";
+
+  std::string str = SRUN + "-N " + PROC_X + "-n " + THREAD_X + " "+ X_PATH + X_OPT;
   const char *command = str.c_str(); 
   system(command);
-  system("../../benchmark/XGC/bin/coupling");
-/*  run_coupling beta(lib1);
-  beta.check_lib(lib1);
-  std::cout << "ls'ing /" << std::endl;
-  std::cout << "returned: " << beta.ls("/") << std::endl;*/
+
+  //system("wait");
+
+  str = SRUN + "-N " + PROC_G + "-n " + THREAD_G + G_PATH + G_OPT;
+  command = str.c_str(); 
+  system(command);
+//	                  -----> XGC 
+//                       | 
+// -----> BENCHMARK ---->
+// (coupling binary loc) |
+// 	                  -----> GENE
+
   return 0;
 
 // MPI_Finalize();

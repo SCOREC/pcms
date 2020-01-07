b81fdf6831cecef43b88593657d06e1f67fb62e6
Abolaji Adesoji <adesoa@remus.scorec.rpi.edu>
2020-01-06 19:18:11 -0500

    cleaned up

--- XGC_adios2/build/CMakeFiles/xgc_es.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libxgc_es.a"
-)
--- XGC_adios2/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /lore/adesoa/dev/wdmapp_coupling/XGC_adios2
-# Build directory: /lore/adesoa/dev/wdmapp_coupling/XGC_adios2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

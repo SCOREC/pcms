3b74b368fae2aa691d16b0116d3cda377d18f499
Damilare <adesoa@rpi.edu>
2020-01-14 15:58:33 -0500

    updating the 3-executable ADIOS2 examples

--- examples/app1/app1.cc
+++ examples/app1/app1.cc
@@ -4,44 +4,49 @@
 #include<adios2.h>
 #include<mpi.h>
 
-void send(int rank, int size);
+void write_density(const std::vector<float> &density, int rank, int size);
+void read_field(std::vector<float> &dens, int rank, int size);
 
 int main(int argc, char *argv[])
 {
   int rank , size;
+  std::vector<float> field = {0.0};
 
-
-  return 0;
-}
-
-
-void send(int rank, int size)
-{
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);   
 
-  std::vector<float> myFloats = {
+  std::vector<float> density = {
     (float)10.0 * rank + 0, (float)10.0 * rank + 1, (float)10.0 * rank + 2,
     (float)10.0 * rank + 3, (float)10.0 * rank + 4, (float)10.0 * rank + 5,
     (float)10.0 * rank + 6, (float)10.0 * rank + 7, (float)10.0 * rank + 8,
     (float)10.0 * rank + 9};
-  const std::size_t Nx = myFloats.size();
+
+  write_density(density, rank, size);
+  read_field(field, rank, size);
+ MPI_Finalize();
+return 0;
+}
+
+
+void write_density(const std::vector<float> &density, int rank, int size)
+{
+ const std::size_t Nx = density.size();
 
   try
   {
-    adios2::ADIOS adios(MPI_COMM_WORLD, adios2::DebugON);
-    adios2::IO sstIO = adios.DeclareIO("myIO");
-    sstIO.SetEngine("Sst");
+    adios2::ADIOS gc_adios(MPI_COMM_WORLD, adios2::DebugON);
+    adios2::IO gdensIO = gc_adios.DeclareIO("gcIO");
+    gdensIO.SetEngine("Sst");
 
-    auto bpFloats = sstIO.DefineVariable<float>("bpFloats", {size * Nx}, {rank * Nx}, {Nx});
+    auto bp_gdens = gdensIO.DefineVariable<float>("bp_gdens", {size * Nx}, {rank * Nx}, {Nx});
 
-    adios2::Engine sstWriter = sstIO.Open("helloSst", adios2::Mode::Write);
+    adios2::Engine gdensWriter = gdensIO.Open("gdens.bp", adios2::Mode::Write);
 
-    sstWriter.BeginStep();
-    sstWriter.Put<float>(bpFloats, myFloats.data());
-    sstWriter.EndStep();
-    sstWriter.Close();
+    gdensWriter.BeginStep();
+    gdensWriter.Put<float>(bp_gdens, density.data());
+    gdensWriter.EndStep();
+    gdensWriter.Close();
   }
 
   catch(std::invalid_argument &e)
@@ -61,6 +66,55 @@ void send(int rank, int size)
     std::cout << "Exception, STOPPING PROGRAM from rank " << rank << "\n";
     std::cout << e.what() << "\n";
   }
+}
 
+void read_field(std::vector<float> &field, int rank, int size)
+{
+    try
+    {
+        adios2::ADIOS cg_adios(MPI_COMM_WORLD, adios2::DebugON);
+        adios2::IO cfieldIO = cg_adios.DeclareIO("cgIO");
+        cfieldIO.SetEngine("Sst");
 
+        adios2::Engine cfieldReader = cfieldIO.Open("cfield.bp", adios2::Mode::Read);
+        cfieldReader.BeginStep();
+
+        adios2::Variable<float> bp_cfield = cfieldIO.InquireVariable<float>("bp_cfield");
+        std::cout << "Incoming variable is of size " << bp_cfield.Shape()[0] << "\n";
+
+        const std::size_t total_size = bp_cfield.Shape()[0];
+        const std::size_t my_start = (total_size / size) * rank;
+        const std::size_t my_count = (total_size / size);
+        std::cout << "cField Reader of rank " << rank << " reading " << my_count
+                  << " floats starting at element " << my_start << "\n";
+
+        const adios2::Dims start{my_start};
+        const adios2::Dims count{my_count};
+
+        const adios2::Box<adios2::Dims> sel(start, count);
+        field.resize(my_count);
+
+        bp_cfield.SetSelection(sel);
+        cfieldReader.Get(bp_cfield, field.data());
+        cfieldReader.EndStep();
+        cfieldReader.Close();
+    }
+    catch (std::invalid_argument &e)
+    {
+        std::cout << "Invalid argument exception, STOPPING PROGRAM from rank "
+                  << rank << "\n";
+        std::cout << e.what() << "\n";
+    }
+    catch (std::ios_base::failure &e)
+    {
+        std::cout << "IO System base failure exception, STOPPING PROGRAM "
+                     "from rank "
+                  << rank << "\n";
+        std::cout << e.what() << "\n";
+    }
+    catch (std::exception &e)
+    {
+        std::cout << "Exception, STOPPING PROGRAM from rank " << rank << "\n";
+        std::cout << e.what() << "\n";
+    }
 }
--- examples/app2/app2.cc
+++ examples/app2/app2.cc
@@ -1,67 +1,119 @@
-#include<chrono>
-#include<numeric>
 #include<iostream>
-#include<thread>
 #include<vector>
 
 #include<adios2.h>
 #include<mpi.h>
 
+void read_density(std::vector<float> &density, int rank, int size);
+void write_field(const std::vector<float> &field, int rank, int size);
+
 int main(int argc, char *argv[])
 {
-	int rank, size;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
-	std::vector<float> myFloats(10);
+  int rank , size;
+  std::vector<float> density;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);   
 
-	try
-	{
-        adios2::ADIOS adios(MPI_COMM_WORLD, adios2::DebugON);
-	adios2::IO sstIO = adios.DeclareIO("myIO");
-	sstIO.SetEngine("Sst");
+  read_density(density, rank, size);
+  // returning the same varaible density across the loop
+  write_field(density, rank, size);
+  MPI_Finalize();
 
-	adios2::Engine sstReader = sstIO.Open("helloSst", adios2::Mode::Read);
-	sstReader.BeginStep();
-	adios2::Variable<float> bpFloats = sstIO.InquireVariable<float>("bpFloats");
-	std::cout << "Incoming variable is of size " << bpFloats.Shape()[0] << "\n";
-
-	const std::size_t total_size = bpFloats.Shape()[0];
-	const std::size_t my_start = (total_size / size) * rank;
-	const std::size_t my_count = (total_size / size);
-	std::cout << "Reader rank " << rank << " reading " << my_count << " floats starting at element " << my_start << "\n";
-	
-	const adios2::Dims start{my_start};
-	const adios2::Dims count{my_count};
-
-	const adios2::Box<adios2::Dims> sel(start, count);
-	std::vector<float> myFloats;
-
-	myFloats.resize(my_count);
-
-	bpFloats.SetSelection(sel);
-	sstReader.Get(bpFloats, myFloats.data());
-	sstReader.EndStep();
-
-
-	sstReader.Close();
-	}
-	catch(std::invalid_argument &e)
-	{
-	  std::cout << "Invalid argument exception, STOPPING PROGRAM from rank " << rank << "\n";
-	  std::cout << e.what() << "\n";
-	}
-	catch(std::ios_base::failure &e)
-	{
-	  std::cout << "IO System base failure exception, STOPPING PROGRAM from rank " << rank << "\n";
-	  std::cout << e.what() << "\n";
-	}
-	catch(std::exception &e)
-	{
-	  std::cout << "Exception, STOPPING PROGRAM from rank " << rank << "\n";
-	  std::cout << e.what() << "\n";
-	}
-	MPI_Finalize();
-	return 0;
+  return 0;
 }
 
+
+void write_field(const std::vector<float> &field, int rank, int size)
+{
+  const std::size_t Nx = field.size();
+
+  try
+  {
+  	adios2::ADIOS xc_adios(MPI_COMM_WORLD, adios2::DebugON);
+  	adios2::IO xfieldIO = xc_adios.DeclareIO("xcIO");
+  	xfieldIO.SetEngine("Sst");
+
+  	auto bp_xfield = xfieldIO.DefineVariable<float>("bp_xfield", {size * Nx}, {rank * Nx}, {Nx});
+
+  	adios2::Engine xfieldWriter = xfieldIO.Open("xfield.bp", adios2::Mode::Write);
+
+  	xfieldWriter.BeginStep();
+  	xfieldWriter.Put<float>(bp_xfield, field.data());
+  	xfieldWriter.EndStep();
+  	xfieldWriter.Close();
+  }
+
+  catch(std::invalid_argument &e)
+  {
+  	std::cout << "Invalid argumant exception, STOPPING PROGRAM from rank "
+  		<< rank << "\n";
+  	std::cout << e.what() << "\n";
+  }
+  catch(std::ios_base::failure &e)
+  {
+  	std::cout << "IO system base failure exception, STOPPING PROGRAM from rank "
+  		<< rank << "\n";
+  	std::cout << e.what() << "\n";
+  }
+  catch(std::exception &e)
+  {
+  	std::cout << "Exception, STOPPING PROGRAM from rank " << rank << "\n";
+  	std::cout << e.what() << "\n";
+  }
+}
+
+void read_density(std::vector<float> &dens, int rank, int size)
+{
+
+  try
+  {
+  	adios2::ADIOS xc_adios(MPI_COMM_WORLD, adios2::DebugON);
+
+  	adios2::IO cdensIO = xc_adios.DeclareIO("xcIO");
+  	cdensIO.SetEngine("Sst");
+
+  	adios2::Engine cdensReader = cdensIO.Open("cdens.bp", adios2::Mode::Read);
+  	cdensReader.BeginStep();
+  	adios2::Variable<float> bp_cdens =
+  		cdensIO.InquireVariable<float>("bp_cdens");
+  	std::cout << "Incoming variable is of size " << bp_cdens.Shape()[0]
+  		<< "\n";
+  	const std::size_t total_size = bp_cdens.Shape()[0];
+  	const std::size_t my_start = (total_size / size) * rank;
+  	const std::size_t my_count = (total_size / size);
+  	std::cout << "cDensity Reader of rank " << rank << " reading " << my_count
+  		<< " floats starting at element " << my_start << "\n";
+
+  	const adios2::Dims start{my_start};
+  	const adios2::Dims count{my_count};
+
+  	const adios2::Box<adios2::Dims> sel(start, count);
+
+  	dens.resize(my_count);
+
+  	bp_cdens.SetSelection(sel);
+  	cdensReader.Get(bp_cdens, dens.data());
+  	cdensReader.EndStep();
+
+  	cdensReader.Close();
+  }
+  catch (std::invalid_argument &e)
+  {
+  	std::cout << "Invalid argument exception, STOPPING PROGRAM from rank "
+  		<< rank << "\n";
+  	std::cout << e.what() << "\n";
+  }
+  catch (std::ios_base::failure &e)
+  {
+  	std::cout << "IO System base failure exception, STOPPING PROGRAM "
+  		"from rank "
+  		<< rank << "\n";
+  	std::cout << e.what() << "\n";
+  }
+  catch (std::exception &e)
+  {
+  	std::cout << "Exception, STOPPING PROGRAM from rank " << rank << "\n";
+  	std::cout << e.what() << "\n";
+  }
+}

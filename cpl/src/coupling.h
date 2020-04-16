ce6b76b293d211494432717c3cd5b630110d3b04
Damilare <adesojidiekola@gmail.com>
2020-04-16 10:49:00 -0400

    compiling successfully - without importpartmesh.cc

--- cpl/src/coupling.h
+++ cpl/src/coupling.h
@@ -91,7 +91,7 @@ namespace coupler {
   /** Destroy Array1d and Array2d objects
    */
   template<typename T>
-    void destroy(Array1d< T>* a) {
+    void destroy(Array1d<T>* a) {
       delete a;
     }
 
@@ -281,48 +281,18 @@ namespace coupler {
     return receive1d_from_ftn<T>(cce_folder,name, io, engine);
   }
   
-  /** Receive density from GENE
-   */
-  template<typename T>
-  Array2d<T>* receive_density(const std::string cce_folder,
-      adios2::IO &io, adios2::Engine &engine) {
-    const std::string name = "gene_density";
-    return receive2d_from_ftn<T>(cce_folder,name, io, engine);
-  }
-  
-  /** Send density to XGC
-   */
-  template<typename T>
-  void send_density(const std::string cce_folder, const Array2d<T>* density,
-      adios2::IO &io, adios2::Engine &engine, adios2::Variable<T> &send_id) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    const std::string fld_name = "cpl_density";
-    send2d_from_C(density, cce_folder, fld_name, io, engine, send_id);
-    std::cerr << rank <<  ": send " << fld_name <<" done \n";
-  }
-  
-  /** Receive field from XGC
-   */
-  template<typename T>
-  Array2d<T>* receive_field(const std::string cce_folder,
-      adios2::IO &io, adios2::Engine &eng) {
-    const std::string name = "xgc_field";
-    return receive2d_from_ftn<T>(cce_folder,name, io, eng);
-  }
-  
-  /** Send field to XGC
-   */
-  template<typename T>
-  void send_field(const std::string cce_folder, const Array2d<T>* field,
-      adios2::IO &io, adios2::Engine &engine, adios2::Variable<T> &send_id) {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    const std::string fld_name = "cpl_field";
-    send2d_from_C(field, cce_folder, fld_name, io, engine, send_id);
-    std::cerr << rank <<  ": send " << fld_name <<" done \n";
-  }
-  
+
+  Array2d<double>* receive_density(const std::string cce_folder,
+  		    adios2::IO &io, adios2::Engine &engine);
+
+  void send_density(const std::string cce_folder, const Array2d<double>* density,
+      adios2::IO &io, adios2::Engine &engine, adios2::Variable<double> &send_id);
+
+  Array2d<double>* receive_field(const std::string cce_folder,
+      adios2::IO &io, adios2::Engine &eng);
+
+  void send_field(const std::string cce_folder, const Array2d<double>* field,
+      adios2::IO &io, adios2::Engine &engine, adios2::Variable<double> &send_id); 
 
   /** Close the Adios2 engine objects
    */

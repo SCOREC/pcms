06f8c9ee542402b70f3eab7bd87e89afc0ac2f51
Damilare <adesojidiekola@gmail.com>
2020-04-16 10:49:00 -0400

    compiling successfully - without importpartmesh.cc

--- cpl/src/coupling.cc
+++ cpl/src/coupling.cc
@@ -3,10 +3,46 @@
 namespace coupler {
 
 
-void close_engines(adios2::Engine engine[], const int num) {
-  for(int i = 0; i < num; i++) {
-    engine[i].Close();
+  Array1d<double>* receive_gene_pproc(const std::string cce_folder,
+      adios2::IO &io, adios2::Engine &engine) {
+    const std::string name = "gene_pproc";
+    return receive1d_from_ftn<double>(cce_folder,name, io, engine);
   }
-}
+  
+  Array2d<double>* receive_density(const std::string cce_folder,
+  		    adios2::IO &io, adios2::Engine &engine) {
+    const std::string name = "gene_density";
+    return receive2d_from_ftn<double>(cce_folder,name, io, engine);
+  }
+
+  void send_density(const std::string cce_folder, const Array2d<double>* density,
+      adios2::IO &io, adios2::Engine &engine, adios2::Variable<double> &send_id) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    const std::string fld_name = "cpl_density";
+    send2d_from_C(density, cce_folder, fld_name, io, engine, send_id);
+    std::cerr << rank <<  ": send " << fld_name <<" done \n";
+  }
+
+  Array2d<double>* receive_field(const std::string cce_folder,
+      adios2::IO &io, adios2::Engine &eng) {
+    const std::string name = "xgc_field";
+    return receive2d_from_ftn<double>(cce_folder,name, io, eng);
+  }
+
+  void send_field(const std::string cce_folder, const Array2d<double>* field,
+      adios2::IO &io, adios2::Engine &engine, adios2::Variable<double> &send_id) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    const std::string fld_name = "cpl_field";
+    send2d_from_C(field, cce_folder, fld_name, io, engine, send_id);
+    std::cerr << rank <<  ": send " << fld_name <<" done \n";
+  }
+
+  void close_engines(adios2::Engine engine[], const int num) {
+    for(int i = 0; i < num; i++) {
+      engine[i].Close();
+    }
+  }
 
 }
--- cpl/src/cpl.cc
+++ cpl/src/cpl.cc
@@ -39,14 +39,14 @@ int main(int argc, char **argv){
 
   for (int i = 0; i < time_step; i++) {
     for (int j = 0; j < RK_count; j++) {
-      coupler::Array2d* density = coupler::receive_density(dir, IO[2], eng[2]);
+      coupler::Array2d<double>* density = coupler::receive_density(dir, IO[2], eng[2]);
       coupler::printSomeDensityVals(density);
       coupler::send_density(dir, density, IO[3], eng[3], send_var[0]);
-   //   coupler::destroy(density);
+      coupler::destroy(density);
 
-      coupler::Array2d* field = coupler::receive_field(dir, IO[4], eng[4]);
+      coupler::Array2d<double>* field = coupler::receive_field(dir, IO[4], eng[4]);
       coupler::send_field(dir, field, IO[5], eng[5], send_var[1]);
- //     coupler::destroy(field);
+      coupler::destroy(field);
     }
   }
 

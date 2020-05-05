cb8acd4f4fec0dd69e47d5d457798a81aced574b
Cameron Smith <smithc11@rpi.edu>
2020-05-04 22:43:10 -0400

    refactor: reformatting a few files
    Used clang-format and then manually modified a few lines.  We'll have to decide on a style.  Using the attached gene file (header sorting off) is fine with me.

--- src/dataprocess.h
+++ src/dataprocess.h
@@ -4,84 +4,94 @@
 #include "couplingTypes.h"
 #include <fftw3.h>
 
-//FIXME indentation is wrong
+// FIXME indentation is wrong
 namespace coupler {
 
- //forward declare
- class Part3Mesh3D;
- class Part1ParalPar3D;
- class BoundaryDescr3D;
+// forward declare
+class Part3Mesh3D;
+class Part1ParalPar3D;
+class BoundaryDescr3D;
 
- class DatasProc3D{
-   public:
-     LO part1li0; // part1li0 is the element number of subdomain of x on each process belonging to comm_y. This x subdomain
-                // belongs to 2d box on the x-y plane storing the input charged ion density for fourier transform.
-     LO part3li0; // part3li0 is the element number of subdomain of x on each process belonging to comm_y. This x subdomain
-                // belongs to 2d box on the x-y plane storing the input electrostatic potential.
-     LO part1lj0; //the count of elements on y domain on each process after backward Fourier transform
-     LO part3lj0; ////the count of elements on y domain on each process after forward Fourier transform
- 
+class DatasProc3D {
+public:
+  LO part1li0; // part1li0 is the element number of subdomain of x on each
+               // process belonging to comm_y. This x subdomain belongs to 2d
+               // box on the x-y plane storing the input charged ion density for
+               // fourier transform.
+  LO part3li0; // part3li0 is the element number of subdomain of x on each
+               // process belonging to comm_y. This x subdomain belongs to 2d 
+               // box on the x-y plane storing the input electrostatic 
+               // potential.
+  LO part1lj0; // the count of elements on y domain on each process after
+               // backward Fourier transform
+  LO part3lj0; ////the count of elements on y domain on each process after
+               ///forward Fourier transform
 
-     LO sum;
-// here, pointers must be assigned a NULL;
-     CV*** densin=NULL;  // input 3d density in complex number
-     CV*  densintmp=NULL;  // temporary 2d density array prepared for backward fourier transform
-     double* densouttmp=NULL; // store the x-y 2d real density after backward fourier transform
-     double*** densout=NULL;   // store xyz 3d real density
-     double*** denspart3=NULL; // storing the density being sent to the part3 
-     double*** potentin=NULL;   // the input real electrostatic potential in 3d xyz
-     double* potentintmp=NULL;  // temporary xy 2d potential array for forward fourier transform
-     CV* potentouttmp=NULL; //
-     CV*** potentout=NULL; // 3d temporary array stroring complex electrostatic potential
-     CV*** potentpart1=NULL; // storing the electrostatic potential being sent to the part1.
-     fftw_plan plan_forward=NULL, plan_backward=NULL;
-//The following parameters for yparal=true;
-     LO myli0;
-     /* constructor
-      * optional argument supports setting
-      * the prepoc and yparal modes
-      */
-     DatasProc3D(const Part1ParalPar3D &p1pp3d,
-         const Part3Mesh3D &p3m3d,
-         bool pproc = true,
-         bool ypar = false)
-       : preproc(pproc), yparal(ypar) {
-       init(p1pp3d,p3m3d);
-       AllocDensityArrays(p1pp3d,p3m3d);
-       AllocPotentArrays(p1pp3d,p3m3d);
-     }
-     ~DatasProc3D();
-     //routines for Fourier transform
-     void CmplxdataToRealdata3D(const Part1ParalPar3D& p1pp3d);
-     void RealdataToCmplxdata3D(const Part1ParalPar3D& p1pp3d, const Part3Mesh3D& p3m3d);
-     void InitFourierPlan3D(const Part1ParalPar3D& p1pp3d, const Part3Mesh3D &p3m3d);
-   private:
-     const bool preproc;
-     const bool yparal;
-     /* helper function for destructor */
-     void FreeFourierPlan3D(); // called from the destructor - does that make sense?
-     /* helper functions for constructor */
-     void init(const Part1ParalPar3D &p1pp3d, const Part3Mesh3D &p3m3d);
-     void AllocDensityArrays(const Part1ParalPar3D& p1pp3d, const Part3Mesh3D& p3m3d);
-     void AllocPotentArrays(const Part1ParalPar3D& p1pp3d, const Part3Mesh3D &p3m3d);
-     /* helper functions for CmplxdataToRealdata3D and RealdataToCmplxdata3D */
-     void ExecuteRealToCmplx(const Part1ParalPar3D& p1pp3d);
-     void ExecuteCmplxToReal(const Part1ParalPar3D& p1pp3d);
+  LO sum;
+  // here, pointers must be assigned a NULL;
+  CV*** densin = NULL; // input 3d density in complex number
+  CV* densintmp = NULL; // temporary 2d density array prepared for backward
+                        // fourier transform
+  double* densouttmp = NULL; // store the x-y 2d real density after backward 
+                             // fourier transform
+  double*** densout = NULL; // store xyz 3d real density
+  double*** denspart3 = NULL; // storing the density being sent to the part3
+  double*** potentin = NULL; // the input real electrostatic potential in 3d xyz
+  double* potentintmp = NULL; // temporary xy 2d potential array for forward 
+                              // fourier transform
+  CV* potentouttmp = NULL;
+  CV*** potentout = NULL; // 3d temporary array stroring complex
+                          // electrostatic potential
+  CV*** potentpart1 = NULL; // storing the electrostatic potential being sent
+                            // to the part1.
+  fftw_plan plan_forward = NULL, plan_backward = NULL;
+  // The following parameters for yparal=true;
+  LO myli0;
+  /* constructor
+   * optional argument supports setting
+   * the prepoc and yparal modes
+   */
+  DatasProc3D(const Part1ParalPar3D& p1pp3d, const Part3Mesh3D& p3m3d,
+              bool pproc = true, bool ypar = false)
+    : preproc(pproc), yparal(ypar) {
+    init(p1pp3d, p3m3d);
+    AllocDensityArrays(p1pp3d, p3m3d);
+    AllocPotentArrays(p1pp3d, p3m3d);
+  }
+  ~DatasProc3D();
+  // routines for Fourier transform
+  void CmplxdataToRealdata3D(const Part1ParalPar3D& p1pp3d);
+  void RealdataToCmplxdata3D(const Part1ParalPar3D& p1pp3d,
+                             const Part3Mesh3D& p3m3d);
+  void InitFourierPlan3D(const Part1ParalPar3D& p1pp3d,
+                         const Part3Mesh3D& p3m3d);
+
+private:
+  const bool preproc;
+  const bool yparal;
+  /* helper function for destructor */
+  void
+  FreeFourierPlan3D(); // called from the destructor - does that make sense?
+  /* helper functions for constructor */
+  void init(const Part1ParalPar3D& p1pp3d, const Part3Mesh3D& p3m3d);
+  void AllocDensityArrays(const Part1ParalPar3D& p1pp3d,
+                          const Part3Mesh3D& p3m3d);
+  void AllocPotentArrays(const Part1ParalPar3D& p1pp3d,
+                         const Part3Mesh3D& p3m3d);
+  /* helper functions for CmplxdataToRealdata3D and RealdataToCmplxdata3D */
+  void ExecuteRealToCmplx(const Part1ParalPar3D& p1pp3d);
+  void ExecuteCmplxToReal(const Part1ParalPar3D& p1pp3d);
 };
 
-void TransposeComplex(CV** InMatrix,CV** OutMatrix, DatasProc3D& dp3d,
-     Part1ParalPar3D& p1pp3d);
+void TransposeComplex(CV** InMatrix, CV** OutMatrix, DatasProc3D& dp3d,
+                      Part1ParalPar3D& p1pp3d);
 
-void InterpoDensity3D(const BoundaryDescr3D &bdesc,
-    const Part3Mesh3D& p3m3d,
-    const Part1ParalPar3D &p1pp3d,
-    const DatasProc3D& dp3d);
+void InterpoDensity3D(const BoundaryDescr3D& bdesc, const Part3Mesh3D& p3m3d,
+                      const Part1ParalPar3D& p1pp3d, const DatasProc3D& dp3d);
 
-void InterpoPotential3D(const BoundaryDescr3D &bdesc,
-    const Part3Mesh3D& p3m3d,
-    const Part1ParalPar3D &p1pp3d,
-    const DatasProc3D& dp3d);
+void InterpoPotential3D(const BoundaryDescr3D& bdesc, const Part3Mesh3D& p3m3d,
+                        const Part1ParalPar3D& p1pp3d, const DatasProc3D& dp3d);
 
-}
+} // namespace coupler
 
 #endif
--- src/dataprocess_impl.h
+++ src/dataprocess_impl.h
@@ -6,44 +6,44 @@
 
 namespace coupler {
 
-//FIXME Not used; was not compiled; based on the
-//FIXME arguments this should go into DatasProc3D.
-  template<class T>
-void  zDensityBoundaryBufAssign(LO nzb,LO lx,LO ly,LO lz,T*** lowbuf,
-       T*** upbuf,T*** box,Part1ParalPar3D &p1pp3d)
-  {
-   if(lowbuf==NULL||upbuf==NULL){
-     std::cout<<"ERROR:the boundary buffer must be alloctted beforing involing this routine.";
-     std::exit(EXIT_FAILURE);
+// FIXME Not used; was not compiled; based on the
+// FIXME arguments this should go into DatasProc3D.
+template <class T>
+void zDensityBoundaryBufAssign(LO nzb, LO lx, LO ly, LO lz, T*** lowbuf,
+                               T*** upbuf, T*** box, Part1ParalPar3D& p1pp3d) {
+  if (lowbuf == NULL || upbuf == NULL) {
+    std::cout << "ERROR:the boundary buffer must be alloctted beforing "
+                 "involing this routine.";
+    std::exit(EXIT_FAILURE);
   }
-  if(p1pp3d.npz>1){
-    if(lz>=nzb){
-      //SZ The call to mpisendrecv_aux2D was missing the 
-      //SZ mpi communicator argument, nzb, and was passing
-      //SZ p1pp3d which did not match the function definition.
-      MPI_Comm FIXME = MPI_COMM_WORLD; //HACK to get this to compile
-      mpisendrecv_aux2D(FIXME,nzb,lx,ly,lz,lowbuf,upbuf,box);
+  if (p1pp3d.npz > 1) {
+    if (lz >= nzb) {
+      // SZ The call to mpisendrecv_aux2D was missing the
+      // SZ mpi communicator argument, nzb, and was passing
+      // SZ p1pp3d which did not match the function definition.
+      MPI_Comm FIXME = MPI_COMM_WORLD; // HACK to get this to compile
+      mpisendrecv_aux2D(FIXME, nzb, lx, ly, lz, lowbuf, upbuf, box);
     } else {
-      std::cout<<"ERROR: nzb is larger than lzb. Large lzb is required.";
+      std::cout << "ERROR: nzb is larger than lzb. Large lzb is required.";
       std::exit(EXIT_FAILURE);
-      }
-  } else{
-      if(p1pp3d.periods[2]==1){
-        for(LO i=0;i<lx-1;i++){
-          for(LO j=0;j<ly-1;j++){
-            for(LO k=0;k<nzb-1;k++){
-              lowbuf[i][j][k]=box[i][j][lz-nzb+k];
-              upbuf[i][j][k]=box[i][j][k];
-            }
+    }
+  } else {
+    if (p1pp3d.periods[2] == 1) {
+      for (LO i = 0; i < lx - 1; i++) {
+        for (LO j = 0; j < ly - 1; j++) {
+          for (LO k = 0; k < nzb - 1; k++) {
+            lowbuf[i][j][k] = box[i][j][lz - nzb + k];
+            upbuf[i][j][k] = box[i][j][k];
           }
         }
-      } else{
-          std::cout<<"The topology is not right."<<'\n';
-          std::exit(EXIT_FAILURE);
-        }
+      }
+    } else {
+      std::cout << "The topology is not right." << '\n';
+      std::exit(EXIT_FAILURE);
     }
+  }
 }
 
-}
+} // namespace coupler
 
 #endif

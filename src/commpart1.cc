d74b10388eae530ef544c5e834c9bf67ff366d6f
Damilare <adesojidiekola@gmail.com>
2020-05-07 11:18:33 -0400

    modified commpart1 to handle GENE_cuth data
    also passes the unit-test

--- src/commpart1.cc
+++ src/commpart1.cc
@@ -5,8 +5,6 @@ namespace coupler {
 
 void Part1ParalPar3D::initTest0(std::string test_dir)
 {
-  int pe;
-  MPI_Comm_rank(MPI_COMM_WORLD,&pe);
   assert(!test_dir.empty());
 /*
   LO size;
@@ -34,21 +32,13 @@ void Part1ParalPar3D::initTest0(std::string test_dir)
   lk0=data[11];
 
   NP=npx*npy*npz;
-  if(pe==0) fprintf(stderr, " 0.15, npx: %d, npy: %d, npz: %d \n", npx,npy,npz);
   CreateSubCommunicators();
-  if(pe==0) fprintf(stderr, " 0.16 \n");
   li1=mype_x*li0;
-  if(pe==0) fprintf(stderr, " 0.17 \n");
   li2=li1+li0-1;
-  if(pe==0) fprintf(stderr, " 0.18 \n");
   lj1=mype_y*lj0;
-  if(pe==0) fprintf(stderr, " 0.19 \n");
   lj2=lj1+lj0-1;
-  if(pe==0) fprintf(stderr, " 0.20 \n");
   lk1=mype_z*lk0;
-  if(pe==0) fprintf(stderr, " 0.21 \n");
   lk2=lk1+lk0-1;
-  if(pe==0) fprintf(stderr, " 0.22 \n");
   
   delete[] data;
 }
@@ -56,19 +46,10 @@ void Part1ParalPar3D::initTest0(std::string test_dir)
 //read the paralllization parameters
 void Part1ParalPar3D::init(std::string test_dir)
 {
-  int pe;
-  MPI_Comm_rank(MPI_COMM_WORLD,&pe);
-  if(pe==0) fprintf(stderr, " 0.0 ");
-
  if(preproc==true){ 
-//   LO* parpar=new LO[26];   
    if(test_case==TestCase::t0){
-  if(pe==0) fprintf(stderr, " 0.1 ");
      initTest0(test_dir);
-  if(pe==0) fprintf(stderr, " 0.3 ");
    }else{
-  if(pe==0) fprintf(stderr, " 0.2 ");
-//     receive_field1D(GO &parpar, "../coupling","para_parameters",9,MPI_COMM_WORLD);
      npx=parpar->val(0);
      nx0=parpar->val(1);
      nxb=parpar->val(2);
@@ -106,25 +87,42 @@ void Part1ParalPar3D::init(std::string test_dir)
    // initialize the radial locations of the flux surface and poloidal angles
    pzcoords=new double[nz0];
    xcoords=new double[nx0];
+
+   // If running the test case, take this route
    if(test_case==TestCase::t0){
+      double* xzcoord; // this lie may be deleted
+      xzcoord=new double[nx0];
       assert(!test_dir.empty());
       std::string fname=test_dir+"xcoords.nml";
-      InputfromFile_(xzcoords,nx0,fname);
+      InputfromFile(xzcoord,nx0,fname);
+      for(LO i=0;i<nx0;i++){
+        xcoords[i]=xzcoord[i];
+      }
+      if(test_case==TestCase::t0){
+        dz=2.0*cplPI/nz0;
+      }else{
+        dz=xzcoord[nx0-1]; 
+      }
+                                             
+      for(LO i=0;i<nz0;i++){
+        pzcoords[i]=-1.0*cplPI+(double)i*dz;
+      }
+   // if running the GENE_cuth test, take this route
    }else{
+      for(LO i=0;i<nx0;i++){
+        xcoords[i]=xzcoords->val(i);
+      }
+      if(test_case==TestCase::t0){
+        dz=2.0*cplPI/nz0;
+      }else{
+        dz=xzcoords->val(nx0-1);
+      }
+                                                
+      for(LO i=0;i<nz0;i++){
+        pzcoords[i]=-1.0*cplPI+(double)i*dz;
+      }
    }
-
-   for(LO i=0;i<nx0;i++){
-     xcoords[i]=xzcoords->val(i);
-   }
-   if(test_case==TestCase::t0){
-     dz=2.0*cplPI/nz0;
-   }else{
-     dz=xzcoords->val(nx0-1); 
-   }
- 
-   for(LO i=0;i<nz0;i++){
-     pzcoords[i]=-1.0*cplPI+(double)i*dz;
-   }
+   
   delete[] parpar;
   delete[] xzcoords;
  }
@@ -132,43 +130,29 @@ void Part1ParalPar3D::init(std::string test_dir)
 
 void Part1ParalPar3D::CreateSubCommunicators()
 {
-int nprocs;
-  MPI_Comm_rank(MPI_COMM_WORLD,&mype);
-  MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
-MPI_Barrier(MPI_COMM_WORLD);
   npw = 4;//TODO: this should be sent in via ADIOS
    // create 3D parallel cart with z being periodic
    //int rorder = 0;//TODO: Does 0 mean true or false
    int dim[4]={(int)npx,(int)npy,(int)npz, (int)npw};
-   if(mype==0) fprintf(stderr," npx: %d, npy: %d, npz: %d, npw: %d \n", npx, npy, npz, npw); 
    MPI_Cart_create(MPI_COMM_WORLD,4,dim,periods,false,&comm_cart);
-  if(mype==0) fprintf(stderr, " 0.151 ,nprocs:%d \n", nprocs);
-  if(mype==0) fprintf(stderr, " 0.1511 comm_cart:%p \n", comm_cart);
 
    MPI_Comm subcomuni[4];
-  if(mype==0) fprintf(stderr, " 0.152 ");
-   for(int i=0;i<4;i++){
+   for (int i=0; i<4; i++){
      int remain[4]={0,0,0,0};
      remain[i]=1;
      MPI_Cart_sub(comm_cart,remain,&subcomuni[i]);
-     if(mype==0) fprintf(stderr, " 0.153 subcomm[i]: %p \n",&subcomuni[i]);
    }
-   if(mype==0) fprintf(stderr," subcomun[1]: %p, subcomun[2]: %p, subcomun[3]: %p, subcomun[4]: %p \n",subcomuni[0],subcomuni[1],subcomuni[2],subcomuni[3]); 
-  if(mype==0) fprintf(stderr, " 0.154 ");
 
    comm_x=subcomuni[0];
    comm_y=subcomuni[1];
    comm_z=subcomuni[2];
    comm_w=subcomuni[3];
 
-   if(mype==0) fprintf(stderr," comm_x: %p, comm_y: %p, comm_z: %p, comm_w: %p \n ", comm_x ,comm_y , comm_z,comm_w);
    MPI_Comm_rank(MPI_COMM_WORLD,&mype);
    MPI_Comm_rank(comm_x,&mype_x);
    MPI_Comm_rank(comm_y,&mype_y);
    MPI_Comm_rank(comm_z,&mype_z);
    MPI_Comm_rank(comm_w,&mype_w);
-   if(mype==0) fprintf(stderr," mype_x : %d, mype_y : %d, mype_z: %d, mype_w: %d \n ", mype_x, mype_y , mype_z,mype_w);
-
 }
 
 void Part1ParalPar3D::MpiFreeComm()

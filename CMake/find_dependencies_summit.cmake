b9248b9ef693e50695183bf5f5d9dee002e32463
Abolaji Adesoji <adesojidiekola@gmail.com>
2021-01-07 09:14:06 -0500

    building and running on summit

--- CMake/find_dependencies_summit.cmake
+++ CMake/find_dependencies_summit.cmake
@@ -4,7 +4,8 @@ SET(Kokkos_INCLUDE_DIRS_RET "/gpfs/alpine/world-shared/phy122/lib/install/summit
 include_directories(${Kokkos_INCLUDE_DIRS_RET})
 
 set(ADIOS1_ROOT "/ccs/home/shku/Software/install/summit/adios/devel/pgi19.4")
-set(ADIOS2_ROOT "/gpfs/alpine/world-shared/csc143/jyc/summit/sw/adios2/devel/pgi")
+#set(ADIOS2_ROOT "/gpfs/alpine/world-shared/csc143/jyc/summit/sw/adios2/devel/pgi")
+set(ADIOS2_ROOT "/gpfs/alpine/csc143/world-shared/jyc/summit/sw/adios2/v2.6.0-367-g15b8642/pgi19.9")
 
 #set(FFTW3_ROOT "/autofs/nccs-svm1_sw/summit/.swci/1-compute/opt/spack/20180914/linux-rhel7-ppc64le/pgi-19.9/fftw-3.3.8-wri566ow7qlimailhyznou5cfm4h362z")
 

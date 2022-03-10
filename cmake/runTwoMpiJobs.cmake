c10d177e7604714ccb4c8c3782346a7847b4b60b
Cameron Smith <smithc11@rpi.edu>
2022-03-10 08:38:09 -0500

    ctest: support multiple args in dual mpi fns

--- cmake/runTwoMpiJobs.cmake
+++ cmake/runTwoMpiJobs.cmake
@@ -1,21 +1,25 @@
 cmake_minimum_required(VERSION 3.15.0...3.21.0)
-macro(mpi_test PROCS1 EXE1 ARGS1 PROCS2 EXE2 ARGS2)
+macro(mpi_test)
+  set(oneValueArgs EXE1 EXE2 PROCS1 PROCS2)
+  set(multiValueArgs ARGS1 ARGS2)
+  cmake_parse_arguments(MPITEST "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
   execute_process(
     RESULTS_VARIABLE result
-    OUTPUT_VARIABLE stdout
-    ERROR_VARIABLE stderr
+    OUTPUT_VARIABLE stdouterr
+    ERROR_VARIABLE stdouterr
     COMMAND_ECHO STDOUT
-    COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${PROCS1}
+    COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${MPITEST_PROCS1}
             ${VALGRIND} ${VALGRIND_ARGS}
-            ${EXE1} ${ARGS1}
-    COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${PROCS2}
+            ${MPITEST_EXE1} ${MPITEST_ARGS1}
+    COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${MPITEST_PROCS2}
             ${VALGRIND} ${VALGRIND_ARGS}
-            ${EXE2} ${ARGS2}
+            ${MPITEST_EXE2} ${MPITEST_ARGS2}
   )
   foreach(res IN LISTS result)
     message(STATUS "res ${res}")
     if(res)
-      message(FATAL_ERROR "Error stdout ${stdout} stderr ${stderr}")
+      message(STATUS "${stdouterr}")
+      message(FATAL_ERROR "ERROR...")
     endif()
   endforeach()
 endmacro(mpi_test)

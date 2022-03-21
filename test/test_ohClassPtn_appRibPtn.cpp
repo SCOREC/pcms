92e83f32a3a5cd88b3f196853f4a0b94f45b87f0
Cameron Smith <smithc11@rpi.edu>
2022-03-21 16:42:12 -0400

    ribPtn: assert on gid match fails using rdv permutation
    gids_h[rdvPermute[i]] == vtxData[i] failed at test/test_ohClassPtn_appRibPtn.cpp + 155

--- test/test_ohClassPtn_appRibPtn.cpp
+++ test/test_ohClassPtn_appRibPtn.cpp
@@ -74,8 +74,10 @@ void prepareMsg(Omega_h::Mesh& mesh, redev::ClassPtn& ptn,
     assert(destRankCounts.count(dr));
     destRankCounts[dr]++;
   }
-  REDEV_ALWAYS_ASSERT(destRankCounts[0] == 6);
-  REDEV_ALWAYS_ASSERT(destRankCounts[1] == 13);
+
+  //REDEV_ALWAYS_ASSERT(destRankCounts[0] == 6);
+  //REDEV_ALWAYS_ASSERT(destRankCounts[1] == 13);
+
   //create dest and offsets arrays from degree array
   offset.resize(destRankCounts.size()+1);
   dest.resize(destRankCounts.size());
@@ -86,10 +88,12 @@ void prepareMsg(Omega_h::Mesh& mesh, redev::ClassPtn& ptn,
     offset[i] = offset[i-1]+rankCount.second;
     i++;
   }
-  redev::LOs expectedDest = {0,1};
-  REDEV_ALWAYS_ASSERT(dest == expectedDest);
-  redev::LOs expectedOffset = {0,6,19};
-  REDEV_ALWAYS_ASSERT(offset == expectedOffset);
+
+  //redev::LOs expectedDest = {0,1};
+  //REDEV_ALWAYS_ASSERT(dest == expectedDest);
+  //redev::LOs expectedOffset = {0,6,19};
+  //REDEV_ALWAYS_ASSERT(offset == expectedOffset);
+
   //fill permutation array such that for vertex i permute[i] contains the
   //  position of vertex i's data in the message array
   std::map<int,int> destRankIdx;
@@ -105,8 +109,9 @@ void prepareMsg(Omega_h::Mesh& mesh, redev::ClassPtn& ptn,
     auto idx = destRankIdx[dr]++;
     permute[i] = idx;
   }
-  redev::LOs expectedPermute = {0,6,1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18};
-  REDEV_ALWAYS_ASSERT(permute == expectedPermute);
+
+//  redev::LOs expectedPermute = {0,6,1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18};
+//  REDEV_ALWAYS_ASSERT(permute == expectedPermute);
 }
 
 //creates rdvPermute given inGids and the rdv mesh instance
@@ -160,7 +165,9 @@ int main(int argc, char** argv) {
   int nproc = world->size();
   if(argc != 3) {
     std::cerr << "Usage: " << argv[0] << " <1=isRendezvousApp,0=isParticipant> /path/to/omega_h/mesh\n";
-    std::cerr << "WARNING: this test is currently hardcoded for the xgc1_data/Cyclone_ITG/Cyclone_ITG_deltaf_23mesh mesh\n";
+    std::cerr << "WARNING: this test is currently hardcoded for the xgc1_data/Cyclone_ITG/Cyclone_ITG_deltaf_23mesh/mesh.osh\n";
+    std::cerr << "mesh for the rendezvous processes and xgc1_data/Cyclone_ITG/Cyclone_ITG_deltaf_23mesh mesh/2p.osh for the\n";
+    std::cerr << "for the non-rendezvous processes\n";
     exit(EXIT_FAILURE);
   }
   OMEGA_H_CHECK(argc == 3);
@@ -177,10 +184,9 @@ int main(int argc, char** argv) {
     REDEV_ALWAYS_ASSERT(ranks.size()==classIds.size());
     Omega_h::vtk::write_parallel("rdvSplit.vtk", &mesh, mesh.dim());
   } else {
-    //if(!rank) REDEV_ALWAYS_ASSERT(mesh.nelems()=);
-    fprintf(stderr, "%d app nelms %d\n", rank, mesh.nelems());
+    REDEV_ALWAYS_ASSERT(world->size()==2);
+    if(!rank) REDEV_ALWAYS_ASSERT(mesh.nelems()==11);
   }
-  return 0;
   auto ptn = redev::ClassPtn(ranks,classIds);
   redev::Redev rdv(MPI_COMM_WORLD,ptn,isRdv);
   rdv.Setup();
@@ -231,16 +237,16 @@ int main(int argc, char** argv) {
       auto start = std::chrono::steady_clock::now();
       const bool knownSizes = (iter == 0) ? false : true;
       comm.Unpack(rdvSrcRanks,rdvOffsets,msgs,msgStart,msgCount,knownSizes);
-      REDEV_ALWAYS_ASSERT(rdvOffsets == redev::GOs({0,6,19}));
-      REDEV_ALWAYS_ASSERT(rdvSrcRanks == redev::GOs({0,0}));
+      //REDEV_ALWAYS_ASSERT(rdvOffsets == redev::GOs({0,6,19}));
+      //REDEV_ALWAYS_ASSERT(rdvSrcRanks == redev::GOs({0,0}));
       if(!rank) {
-        REDEV_ALWAYS_ASSERT(msgStart==0 && msgCount==6);
+        //REDEV_ALWAYS_ASSERT(msgStart==0 && msgCount==6);
         redev::GOs msgVec(msgs, msgs+msgCount);
-        REDEV_ALWAYS_ASSERT(msgVec == redev::GOs({0,2,3,4,5,6}));
+        //REDEV_ALWAYS_ASSERT(msgVec == redev::GOs({0,2,3,4,5,6}));
       } else {
-        REDEV_ALWAYS_ASSERT(msgStart==6 && msgCount==13);
+        //REDEV_ALWAYS_ASSERT(msgStart==6 && msgCount==13);
         redev::GOs msgVec(msgs, msgs+msgCount);
-        REDEV_ALWAYS_ASSERT(msgVec == redev::GOs({1,7,8,9,10,11,12,13,14,15,16,17,18}));
+        //REDEV_ALWAYS_ASSERT(msgVec == redev::GOs({1,7,8,9,10,11,12,13,14,15,16,17,18}));
       }
       auto end = std::chrono::steady_clock::now();
       std::chrono::duration<double> elapsed_seconds = end-start;

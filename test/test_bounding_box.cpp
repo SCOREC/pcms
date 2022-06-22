c49bda1bb56f2dc6dd80b1cfd0c841cd79307ef6
Jacob Merson <jacob@jacobmerson.com>
2022-06-22 01:11:40 -0400

    bounding box test case (for point search)

---
+++ test/test_bounding_box.cpp
@@ -0,0 +1,23 @@
+#include <catch2/catch.hpp>
+#include <wdmcpl/bounding_box.h>
+
+using wdmcpl::AABBox;
+using wdmcpl::intersects;
+#include <iostream>
+
+TEST_CASE("test intersection") {
+  const AABBox<2> unit_square{.center={0,0}, .half_width={0.5,0.5}};
+  REQUIRE(intersects(unit_square,unit_square));
+  AABBox<2> shifted_unit_square{.center={0,0.4}, .half_width={0.5,0.5}};
+  REQUIRE(intersects(unit_square,shifted_unit_square));
+  shifted_unit_square.center[1] *= 2;
+  REQUIRE(intersects(unit_square,shifted_unit_square));
+  shifted_unit_square.center[1] *= 2;
+  REQUIRE(!intersects(unit_square,shifted_unit_square));
+  // test on negative side
+  shifted_unit_square.center[1] *= -1;
+  REQUIRE(!intersects(unit_square,shifted_unit_square));
+  shifted_unit_square.center[1] /= 2;
+  REQUIRE(intersects(unit_square,shifted_unit_square));
+
+}

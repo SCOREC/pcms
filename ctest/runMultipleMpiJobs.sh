a5142f9b6662d6800763af4fe763f9ce739801e8
Cameron Smith <smithc11@rpi.edu>
2022-07-29 16:45:47 -0400

    valgrind logging by default

--- ctest/runMultipleMpiJobs.sh
+++ ctest/runMultipleMpiJobs.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash -x
 usage="Usage: <run command> <process flag> <valgrindCmd> <valgrindOptions> <name0> <procs0> <exe0> <args0> ... <name2> <procs2> <exe2> <args2>"
 if [[ ($# == 0) || ("${1}" == "-h" || ${1} == "--help") ]]; then
   echo $usage
@@ -8,14 +8,18 @@ runCmd=${1}
 numProcsFlag=${2}
 valgrindCmd=${3}
 valgrindOptions=${4}
+valgrindLogging="--log-file=%p"
 #clear the valgrind args if they are 'none'
 if [[ ${valgrindCmd} == "none" ]]; then
   valgrindCmd=""
+  valgrindOptions=""
+  valgrindLogging=""
 fi
-if [[ ${valgrindOptions} == "none" ]]; then
+if [[ (${valgrindOptions} == "none" && ${valgrindCmd} != "none") ]]; then
   valgrindOptions=""
 fi
 shift 4
+
 
 declare -a PIDS
 declare -a LOGS
@@ -25,7 +29,7 @@ run() {
   local exe=${3}
   local args=${4}
   IFS=';' read -a argsArray <<< "${args}" #split the cmake list of args
-  ${runCmd} ${numProcsFlag} ${procs} ${valgrindCmd} ${valgrindOptions} ${exe} ${argsArray[@]} &> ${name}.log &
+  ${runCmd} ${numProcsFlag} ${procs} ${valgrindCmd} ${valgrindLogging}_${name}.vg ${valgrindOptions} ${exe} ${argsArray[@]} &> ${name}.log &
   PIDS+=($!)
   LOGS+=(${name}.log)
 }

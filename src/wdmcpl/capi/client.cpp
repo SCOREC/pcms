fdbe6fd92c2b1c0ec7fcb409394c335d2c82fdf1
Jacob Merson <jacob@jacobmerson.com>
2023-01-02 03:59:31 -0500

    Use variant for FieldAdapter
    This method is safer than the union that requires correct usage
    everywhere.

--- src/wdmcpl/capi/client.cpp
+++ src/wdmcpl/capi/client.cpp
@@ -1,91 +1,31 @@
 #include "client.h"
 #include "wdmcpl.h"
 #include "wdmcpl/xgc_field_adapter.h"
+#include <variant>
+#include <redev_variant_tools.h>
 // #ifdef WDMCPL_HAS_OMEGA_H
 //   #include "wdmcpl/omega_h_field.h"
 // #endif
 #include <fstream>
 #include "wdmcpl/xgc_reverse_classification.h"
-extern "C" {
-// TODO move into XGC specific header
-struct WdmCplXgcAdapter;
-typedef struct WdmCplXgcAdapter WdmCplXgcAdapter;
-struct WdmCplFieldAdapter
-{
-  WdmCplAdapterType type;
-  WdmCplType data_type;
-  union
-  {
-    WdmCplXgcAdapter* xgc_;
-    // OmegaHAdapter* omega_h;
-    // GENEAdapter* gene_;
-    // GEMAdapter* gem_;
-  };
-};
-typedef struct WdmCplFieldAdapter WdmCplFieldAdapter;
-};
 namespace wdmcpl
 {
-
-template <typename T>
-static WdmCplFieldHandle* wdmcpl_add_field_t(WdmCplClientHandle* client_handle, const char* name,
-                               WdmCplFieldAdapter* adapter_handle)
-{
-  auto* client = reinterpret_cast<wdmcpl::CouplerClient*>(client_handle);
-  CoupledField* field = nullptr;
-  switch (adapter_handle->type) {
-    case WDMCPL_ADAPTER_XGC: {
-      auto* adapter =
-        reinterpret_cast<wdmcpl::XGCFieldAdapter<T>*>(adapter_handle->xgc_);
-      field = client->AddField(name, *adapter);
-      break;
-    }
+// Note that we have a closed set of types that can be used in the C interface
+using FieldAdapterVariant =
+  std::variant<std::monostate, wdmcpl::XGCFieldAdapter<double>,
+               wdmcpl::XGCFieldAdapter<float>, wdmcpl::XGCFieldAdapter<int>,
+               wdmcpl::XGCFieldAdapter<long>
 #ifdef WDMCPL_HAS_OMEGA_H
-    case WDMCPL_ADAPTER_OMEGAH: {
-      if constexpr (std::is_same_v<T, double> || std::is_same_v<T, int>) {
-        auto* adapter = reinterpret_cast<wdmcpl::OmegaHFieldAdapter<T>*>(
-          adapter_handle->xgc_);
-        field = client->AddField(name, *adapter);
-      } else {
-        // error trying to use omega_h adapter with unsupported data type
-        Wdmcpl_Assert_Fail("Omega_h can only use double or int fields.");
-      }
-      break;
-    }
+               ,
+               wdmcpl::OmegaHFieldAdapter<double>,
+               wdmcpl::OmegaHFieldAdapter<int>
 #endif
-    case WDMCPL_ADAPTER_GENE:
-    case WDMCPL_ADAPTER_GEM:
-      Wdmcpl_Assert_Fail("Unhandled field adapter type in C interface\n");
-  }
-  return reinterpret_cast<WdmCplFieldHandle*>(field);
-}
-
-template <typename F>
-auto ApplyToTypes(WdmCplType type, const F&& f)
-{
-  switch (type) {
-    case WDMCPL_DOUBLE: {
-      return f(double{});
-    }
-    case WDMCPL_FLOAT: {
-      return f(float{});
-    }
-    case WDMCPL_INT: {
-      return f(int{});
-    }
-    case WDMCPL_LONG_INT: {
-      return f(long{});
-    }
-  }
-  // quash compiler warning
-  using dummy_type = typename std::result_of_t<decltype(f)(int)>;
-  return static_cast<dummy_type>(nullptr);
-}
+               >;
 
 } // namespace wdmcpl
 
 [[nodiscard]] WdmCplClientHandle* wdmcpl_create_client(const char* name,
-                                                 MPI_Comm comm)
+                                                       MPI_Comm comm)
 {
   auto* client = new wdmcpl::CouplerClient(name, comm);
   return reinterpret_cast<WdmCplClientHandle*>(client);
@@ -103,99 +43,97 @@ WdmCplReverseClassificationHandle* wdmcpl_load_reverse_classification(
     wdmcpl::ReadReverseClassificationVertex(filepath, comm)};
   return reinterpret_cast<WdmCplReverseClassificationHandle*>(rc);
 }
-void wdmcpl_destroy_reverse_classification(WdmCplReverseClassificationHandle* rc)
+void wdmcpl_destroy_reverse_classification(
+  WdmCplReverseClassificationHandle* rc)
 {
-  if(rc != nullptr)
+  if (rc != nullptr)
     delete reinterpret_cast<wdmcpl::ReverseClassificationVertex*>(rc);
 }
 
-WdmCplFieldHandle* wdmcpl_add_field(WdmCplClientHandle* client_handle, const char* name,
-                      WdmCplFieldAdapterHandle* adapter_handle)
+WdmCplFieldHandle* wdmcpl_add_field(WdmCplClientHandle* client_handle,
+                                    const char* name,
+                                    WdmCplFieldAdapterHandle* adapter_handle)
 {
 
-  WdmCplFieldHandle* field = nullptr;
-  auto* adapter = reinterpret_cast<WdmCplFieldAdapter*>(adapter_handle);
-  wdmcpl::ApplyToTypes(adapter->data_type, [&](auto d) {
-    using T = decltype(d);
-    field = wdmcpl::wdmcpl_add_field_t<T>(client_handle, name, adapter);
-  });
-  return field;
+  auto* adapter =
+    reinterpret_cast<wdmcpl::FieldAdapterVariant*>(adapter_handle);
+  auto* client = reinterpret_cast<wdmcpl::CouplerClient*>(client_handle);
+  WDMCPL_ALWAYS_ASSERT(adapter != nullptr);
+  wdmcpl::CoupledField* field = std::visit(
+    redev::overloaded{
+      [](const std::monostate&) -> wdmcpl::CoupledField* { return nullptr; },
+      [&name, &client](const auto& field_adapter) {
+        return client->AddField(name, field_adapter);
+      }},
+    *adapter);
+  return reinterpret_cast<WdmCplFieldHandle*>(field);
 }
 void wdmcpl_send_field_name(WdmCplClientHandle* client_handle, const char* name)
 {
   auto* client = reinterpret_cast<wdmcpl::CouplerClient*>(client_handle);
   client->SendField(name);
 }
-void wdmcpl_receive_field_name(WdmCplClientHandle* client_handle, const char* name)
+void wdmcpl_receive_field_name(WdmCplClientHandle* client_handle,
+                               const char* name)
 {
   auto* client = reinterpret_cast<wdmcpl::CouplerClient*>(client_handle);
   client->ReceiveField(name);
 }
-void wdmcpl_send_field(WdmCplFieldHandle* field_handle) {
-  auto * field = reinterpret_cast<wdmcpl::CoupledField*>(field_handle);
+void wdmcpl_send_field(WdmCplFieldHandle* field_handle)
+{
+  auto* field = reinterpret_cast<wdmcpl::CoupledField*>(field_handle);
   field->Send();
 }
-void wdmcpl_receive_field(WdmCplFieldHandle* field_handle) {
-  auto * field = reinterpret_cast<wdmcpl::CoupledField*>(field_handle);
+void wdmcpl_receive_field(WdmCplFieldHandle* field_handle)
+{
+  auto* field = reinterpret_cast<wdmcpl::CoupledField*>(field_handle);
   field->Receive();
+}
+template <typename T>
+void wdmcpl_create_xgc_field_adapter_t(
+  const char* name, void* data, int size,
+  const wdmcpl::ReverseClassificationVertex& reverse_classification,
+  in_overlap_function in_overlap, wdmcpl::FieldAdapterVariant& field_adapter)
+{
+  wdmcpl::ScalarArrayView<T, wdmcpl::HostMemorySpace> data_view(
+    reinterpret_cast<T*>(data), size);
+  field_adapter.emplace<wdmcpl::XGCFieldAdapter<T>>(
+    name, data_view, reverse_classification, in_overlap);
 }
 WdmCplFieldAdapterHandle* wdmcpl_create_xgc_field_adapter(
   const char* name, void* data, int size, WdmCplType data_type,
   const WdmCplReverseClassificationHandle* rc, in_overlap_function in_overlap)
 {
-  // auto classification = wdmcpl::ReadXGCNodeClassification(infile);
-  auto* field_adapter = new WdmCplFieldAdapter;
-  field_adapter->type = WDMCPL_ADAPTER_XGC;
+  auto* field_adapter = new wdmcpl::FieldAdapterVariant{};
   WDMCPL_ALWAYS_ASSERT(rc != nullptr);
   auto* reverse_classification =
     reinterpret_cast<const wdmcpl::ReverseClassificationVertex*>(rc);
-  switch (data_type) {
-    case WDMCPL_DOUBLE: field_adapter->data_type = WDMCPL_DOUBLE; break;
-    case WDMCPL_FLOAT: field_adapter->data_type = WDMCPL_FLOAT; break;
-    case WDMCPL_INT: field_adapter->data_type = WDMCPL_INT; break;
-    case WDMCPL_LONG_INT: field_adapter->data_type = WDMCPL_LONG_INT; break;
-  }
   WDMCPL_ALWAYS_ASSERT(reverse_classification != nullptr);
-  wdmcpl::ApplyToTypes(data_type, [&](auto d) {
-    using T = decltype(d);
-    wdmcpl::ScalarArrayView<T, wdmcpl::HostMemorySpace> data_view(
-      reinterpret_cast<T*>(data), size);
-    auto* adapter =
-      new wdmcpl::XGCFieldAdapter<T>(name, data_view, *reverse_classification, in_overlap);
-    // auto* adapter = new wdmcpl::XGCFieldAdapter<T>(
-    //   name, std::move(classification.dimension),
-    //   std::move(classification.geometric_id), data_view);
-    field_adapter->xgc_ = reinterpret_cast<WdmCplXgcAdapter*>(adapter);
-  });
+  switch (data_type) {
+    case WDMCPL_DOUBLE:
+      wdmcpl_create_xgc_field_adapter_t<double>(
+        name, data, size, *reverse_classification, in_overlap, *field_adapter);
+      break;
+    case WDMCPL_FLOAT:
+      wdmcpl_create_xgc_field_adapter_t<float>(
+        name, data, size, *reverse_classification, in_overlap, *field_adapter);
+      break;
+    case WDMCPL_INT:
+      wdmcpl_create_xgc_field_adapter_t<int>(
+        name, data, size, *reverse_classification, in_overlap, *field_adapter);
+      break;
+    case WDMCPL_LONG_INT:
+      wdmcpl_create_xgc_field_adapter_t<long int>(
+        name, data, size, *reverse_classification, in_overlap, *field_adapter);
+      break;
+  }
   return reinterpret_cast<WdmCplFieldAdapterHandle*>(field_adapter);
 }
 
-static void wdmcpl_destroy_xgc_adapter(WdmCplXgcAdapter* xgc_adapter,
-                                       WdmCplType data_type)
-{
-  if (xgc_adapter == nullptr) {
-    return;
-  }
-  wdmcpl::ApplyToTypes(data_type, [&xgc_adapter](auto d) {
-    using T = decltype(d);
-    delete reinterpret_cast<wdmcpl::XGCFieldAdapter<T>*>(xgc_adapter);
-    xgc_adapter = nullptr;
-  });
-}
 void wdmcpl_destroy_field_adapter(WdmCplFieldAdapterHandle* adapter_handle)
 {
-  auto* adapter = reinterpret_cast<WdmCplFieldAdapter*>(adapter_handle);
-  switch (adapter->type) {
-    case (WDMCPL_ADAPTER_XGC):
-      wdmcpl_destroy_xgc_adapter(adapter->xgc_, adapter->data_type);
-      break;
-#ifdef WDMCPL_HAS_OMEGA_H
-    case (WDMCPL_ADAPTER_OMEGAH): break;
-#endif
-    case (WDMCPL_ADAPTER_GENE): break;
-    case (WDMCPL_ADAPTER_GEM): break;
-  }
-
+  auto* adapter =
+    reinterpret_cast<wdmcpl::FieldAdapterVariant*>(adapter_handle);
   if (adapter != nullptr) {
     delete adapter;
     adapter = nullptr;

e26a687eca01c1f199ecf6004a72ef32c600633b
Jacob Merson <mersoj@rpi.edu>
2023-03-06 02:04:50 -0500

    first attempt at n0 coupler

--- test/xgc_n0_coupling_server.cpp
+++ test/xgc_n0_coupling_server.cpp
@@ -27,6 +27,51 @@ namespace ts = test_support;
 // TODO: we should communicate the geometric ids in the overlap regions.
 // is there a way to use the isOverlap functor to do this. This allows for
 // maximum flexibility moving forward
+//
+
+[[nodiscard]]
+static wdmcpl::ConvertibleCoupledField* AddField(wdmcpl::CouplerServer &coupler, const std::string& name, const std::string& path, Omega_h::Read<Omega_h::I8> is_overlap, const std::string& numbering, Omega_h::Mesh& mesh, int plane) {
+
+      std::stringstream field_name;
+      field_name << path << name << "_" << plane;
+      return coupler.AddField(field_name.str(),
+                   wdmcpl::OmegaHFieldAdapter<wdmcpl::Real>(
+                     field_name.str(), mesh, is_overlap, numbering),
+                   FieldTransferMethod::Copy, // to Omega_h
+                   FieldEvaluationMethod::None,
+                   FieldTransferMethod::Copy, // from Omega_h
+                   FieldEvaluationMethod::None, is_overlap, path);
+}
+
+struct XGCAnalysis {
+  using FieldVec = std::vector<wdmcpl::ConvertibleCoupledField*>;
+  std::array<FieldVec,2> dpot;
+  FieldVec pot0;
+  std::array<FieldVec,2> edensity;
+  std::array<FieldVec,2> idensity;
+};
+
+static void ReceiveFields(const std::vector<wdmcpl::ConvertibleCoupledField*> & fields) {
+  for(auto* field : fields) {
+    field->Receive();
+  }
+}
+static void SendFields(const std::vector<wdmcpl::ConvertibleCoupledField*> & fields) {
+  for(auto* field : fields) {
+    field->Send();
+  }
+}
+static void CopyFields(const std::vector<wdmcpl::ConvertibleCoupledField*> & from_fields,
+                       const std::vector<wdmcpl::ConvertibleCoupledField*> & to_fields) {
+  WDMCPL_ALWAYS_ASSERT(from_fields.size() == to_fields.size());
+  for(size_t i=0; i<from_fields.size(); ++i) {
+    const auto* from = from_fields[i]->GetFieldAdapter<wdmcpl::OmegaHFieldAdapter<wdmcpl::Real>>();
+    auto* to = to_fields[i]->GetFieldAdapter<wdmcpl::OmegaHFieldAdapter<wdmcpl::Real>>();
+    copy_field(from->GetField(), to->GetField());
+  }
+}
+
+using GatherScatterFieldsVec = std::vector<std::reference_wrapper<wdmcpl::ConvertibleCoupledField>>;
 
 void omegah_coupler(MPI_Comm comm, Omega_h::Mesh& mesh,
                     std::string_view cpn_file, int nphi)
@@ -70,39 +115,86 @@ void omegah_coupler(MPI_Comm comm, Omega_h::Mesh& mesh,
   elapsed_seconds = time2-time1;
   ts::timeMinMaxAvg(elapsed_seconds.count(), min, max, avg);
   if(!rank) ts::printTime("Initialize Coupler/Mesh", min, max, avg);
-  std::vector<wdmcpl::ConvertibleCoupledField*> potential_fields(nphi * 2);
-  std::vector<wdmcpl::ScatterOperation*> scatter_ops(nphi);
-  std::vector<wdmcpl::GatherOperation*> gather_ops(nphi);
+
+  std::vector<wdmcpl::ScatterOperation*> scatter_density_ops;
+  std::vector<wdmcpl::GatherOperation*> gather_density_ops(nphi);
+  XGCAnalysis core_analysis;
+  XGCAnalysis edge_analysis;
   std::cerr << "ADDING FIELDS\n";
   for (int i = 0; i < nphi; ++i) {
-    std::stringstream field1_name;
-    field1_name << "dpot_plane_" << i;
-    std::cerr << field1_name.str() << "\n";
-    potential_fields[2 * i] =
-      cpl.AddField(field1_name.str(),
-                   wdmcpl::OmegaHFieldAdapter<wdmcpl::Real>(
-                     field1_name.str(), mesh, is_overlap, numbering),
-                   FieldTransferMethod::Copy, // to Omega_h
-                   FieldEvaluationMethod::None,
-                   FieldTransferMethod::Copy, // from Omega_h
-                   FieldEvaluationMethod::None, is_overlap, "deltaf1/");
-    potential_fields[2 * i + 1] =
-      cpl.AddField(field1_name.str(),
-                   wdmcpl::OmegaHFieldAdapter<wdmcpl::Real>(
-                     field1_name.str(), mesh, is_overlap, numbering),
-                   FieldTransferMethod::Copy, // to Omega_h
-                   FieldEvaluationMethod::None,
-                   FieldTransferMethod::Copy, // from Omega_h
-                   FieldEvaluationMethod::None, is_overlap, "deltaf2/");
-    gather_ops[i] = cpl.AddGatherFieldsOp(
-      field1_name.str(),
-      {"deltaf1/" + field1_name.str(), "deltaf2/" + field1_name.str()},
-      "combined" + field1_name.str(), ts::MeanCombiner{}, is_overlap);
-    scatter_ops[i] = cpl.AddScatterFieldsOp(
-      field1_name.str(), "combined" + field1_name.str(),
-      {"deltaf1/" + field1_name.str(), "deltaf2/" + field1_name.str()},
-      is_overlap);
-    std::cerr << field1_name.str() << " DONE\n";
+    core_analysis.dpot[0].push_back(AddField(cpl, "dpot_1_plane", "core/", 
+                                             is_overlap, numbering, mesh, i));
+    core_analysis.dpot[1].push_back(AddField(cpl, "dpot_2_plane", "core/", 
+                                             is_overlap, numbering, mesh, i));
+    core_analysis.pot0.push_back(AddField(cpl, "pot0_plane", "core/",
+                                          is_overlap, numbering, mesh, i));
+    core_analysis.edensity[0].push_back(AddField(cpl, "edensity_1_plane", "core/", 
+                                             is_overlap, numbering, mesh, i));
+    core_analysis.edensity[1].push_back(AddField(cpl, "edensity_2_plane", "core/", 
+                                             is_overlap, numbering, mesh, i));
+    core_analysis.idensity[0].push_back(AddField(cpl, "idensity_1_plane", "core/", 
+                                             is_overlap, numbering, mesh, i));
+    core_analysis.idensity[1].push_back(AddField(cpl, "idensity_2_plane", "core/", 
+                                             is_overlap, numbering, mesh, i));
+
+    edge_analysis.dpot[0].push_back(AddField(cpl, "dpot_1_plane", "edge/", 
+                                             is_overlap, numbering, mesh, i));
+    edge_analysis.dpot[1].push_back(AddField(cpl, "dpot_2_plane", "edge/", 
+                                             is_overlap, numbering, mesh, i));
+    edge_analysis.pot0.push_back(AddField(cpl, "pot0_plane", "edge/",
+                                          is_overlap, numbering, mesh, i));
+    edge_analysis.edensity[0].push_back(AddField(cpl, "edensity_1_plane", "edge/", 
+                                             is_overlap, numbering, mesh, i));
+    edge_analysis.edensity[1].push_back(AddField(cpl, "edensity_2_plane", "edge/", 
+                                             is_overlap, numbering, mesh, i));
+    edge_analysis.idensity[0].push_back(AddField(cpl, "idensity_1_plane", "edge/", 
+                                             is_overlap, numbering, mesh, i));
+    edge_analysis.idensity[1].push_back(AddField(cpl, "idensity_2_plane", "edge/", 
+                                             is_overlap, numbering, mesh, i));
+
+    // gather density from core/edge to density
+    //const std::string edensity_name = "edensity_1_plane_"+std::to_string(i);
+    gather_density_ops.push_back(cpl.AddGatherFieldsOp(
+      "edensity_1_plane_"+std::to_string(i),
+      {*core_analysis.edensity[0].back(),
+       *edge_analysis.edensity[0].back() },
+      "combined_edensity_1_plane_"+std::to_string(i),
+      ts::MeanCombiner{}, is_overlap));
+    gather_density_ops.push_back(cpl.AddGatherFieldsOp(
+      "edensity_2_plane_"+std::to_string(i),
+      {*core_analysis.edensity[1].back(),
+       *edge_analysis.edensity[1].back() },
+      "combined_edensity_2_plane_"+std::to_string(i),
+      ts::MeanCombiner{}, is_overlap));
+    gather_density_ops.push_back(cpl.AddGatherFieldsOp(
+      "idensity_1_plane_"+std::to_string(i),
+      {*core_analysis.idensity[0].back(),
+       *edge_analysis.idensity[0].back() },
+      "combined_idensity_1_plane_"+std::to_string(i),
+      ts::MeanCombiner{}, is_overlap));
+    gather_density_ops.push_back(cpl.AddGatherFieldsOp(
+      "idensity_2_plane_"+std::to_string(i),
+      {*core_analysis.idensity[1].back(),
+       *edge_analysis.idensity[1].back() },
+      "combined_idensity_2_plane_"+std::to_string(i),
+      ts::MeanCombiner{}, is_overlap));
+
+    scatter_density_ops.push_back(cpl.AddScatterFieldsOp(
+      "edensity_1_plane_"+std::to_string(i),
+      "combined_edensity_1_plane_"+std::to_string(i),
+      {*edge_analysis.edensity[0].back()},is_overlap));
+    scatter_density_ops.push_back(cpl.AddScatterFieldsOp(
+      "edensity_2_plane_"+std::to_string(i),
+      "combined_edensity_2_plane_"+std::to_string(i),
+      {*edge_analysis.edensity[1].back()},is_overlap));
+    scatter_density_ops.push_back(cpl.AddScatterFieldsOp(
+      "idensity_1_plane_"+std::to_string(i),
+      "combined_idensity_1_plane_"+std::to_string(i),
+      {*edge_analysis.idensity[0].back()},is_overlap));
+    scatter_density_ops.push_back(cpl.AddScatterFieldsOp(
+      "idensity_2_plane_"+std::to_string(i),
+      "combined_idensity_2_plane_"+std::to_string(i),
+      {*edge_analysis.idensity[1].back()},is_overlap));
   }
   auto time3 = std::chrono::steady_clock::now();
   elapsed_seconds = time3-time2;
@@ -110,28 +202,43 @@ void omegah_coupler(MPI_Comm comm, Omega_h::Mesh& mesh,
   if(!rank) ts::printTime("Add Meshes", min, max, avg);
   while (true) {
     auto sr_time1 = std::chrono::steady_clock::now();
-    for(auto* gather : gather_ops) {
+    // gather density fields (Core+Edge)
+    for(auto* gather : gather_density_ops) {
       gather->Run();
     }
     auto sr_time2 = std::chrono::steady_clock::now();
     elapsed_seconds = sr_time2-sr_time1;
     ts::timeMinMaxAvg(elapsed_seconds.count(), min, max, avg);
     if(!rank) ts::printTime("Gather", min, max, avg);
-    for(auto* scatter : scatter_ops) {
+    // Scatter density field (Edge)
+    for(auto* scatter : scatter_density_ops) {
       scatter->Run();
     }
     auto sr_time3 = std::chrono::steady_clock::now();
     elapsed_seconds = sr_time3-sr_time2;
     ts::timeMinMaxAvg(elapsed_seconds.count(), min, max, avg);
     if(!rank) ts::printTime("Scatter", min, max, avg);
-    // TODO: do this w/o blocking...
-    //for (auto* field : potential_fields) {
-    //  WDMCPL_ALWAYS_ASSERT(field != nullptr);
-    //  field->Receive();
-    //}
-    //for (auto* field : potential_fields) {
-    //  field->Send();
-    //}
+
+    // deal with phi fields (pot0/dpot1/dpot2)
+    // 1. reveive fields from Edge
+    ReceiveFields(edge_analysis.dpot[0]);
+    ReceiveFields(edge_analysis.dpot[1]);
+    ReceiveFields(edge_analysis.pot0);
+    // 2. Copy fields from Edge->Core
+    // Since we know that both native fields are Omega_h,
+    // we don't need to sync to the internal and combined
+    // fields that would be created by using Gather/Scatter operation
+    // This is a bit "hacky" and will use our intimate knowledge of how
+    // OmegaHField adapters work. i.e., every field adapter with the same
+    // name refers to the same OmegaH field
+    CopyFields(edge_analysis.dpot[0], core_analysis.dpot[0]);
+    CopyFields(edge_analysis.dpot[1], core_analysis.dpot[1]);
+    CopyFields(edge_analysis.pot0, core_analysis.pot0);
+    // 3. Send fields to Core
+    SendFields(edge_analysis.dpot[0]);
+    SendFields(edge_analysis.dpot[1]);
+    SendFields(edge_analysis.pot0);
+
   }
 }
 

76abe2f767415c9384956a39609916ca21e5a27d
Angelyr <scardking@gmail.com>
2024-11-07 11:36:18 -0800

    longer timeout

--- test/test_ohClassPtn_appRibPtn.cpp
+++ test/test_ohClassPtn_appRibPtn.cpp
@@ -52,7 +52,7 @@ int main(int argc, char** argv)
   const int rdvRanks = 2;
   const int appRanks = 2;
 
-  adios2::Params params{{"Streaming", "On"}, {"OpenTimeoutSecs", "2"}};
+  adios2::Params params{{"Streaming", "On"}, {"OpenTimeoutSecs", "12"}};
   auto channel =
     rdv.CreateAdiosChannel(name, params, redev::TransportType::BP4);
   auto commPair = channel.CreateComm<redev::GO>(name,rdv.GetMPIComm());

ca33b7af61b5f27503f32ad6911303fae1d08913
Cameron Smith <smithc11@rpi.edu>
2025-02-24 22:43:18 -0500

    WIP: start porting from sprEstimateError, does not compile

--- test/thwaites_adapt.cpp
+++ test/thwaites_adapt.cpp
@@ -16,13 +16,104 @@
 #include <Omega_h_dbg.hpp>
 #include <Omega_h_for.hpp>
 
-#include <pcms/interpolator/mls_interpolation.hpp>
-#include <pcms/interpolator/pcms_interpolator_aliases.hpp>
-
 //detect floating point exceptions
 #include <fenv.h>
 
 using namespace Omega_h;
+
+/* common base for Scalar Integrator. */
+class SInt : public MeshField::Integrator {
+  public:
+    SInt(int order):
+      MeshField::Integrator(order),r(0)
+    {}
+    void reset() {r=0;}
+    MeshField::Real r;
+};
+
+struct Estimation {
+  Omega_h::Mesh& mesh;
+  /* the maximum polynomial order that can be
+     integrated exactly using the input integration points.
+     not necessarily equal to recovered_order, sometimes
+     users give more points than strictly necessary */
+  int integration_order;
+  /* the polynomial order of the recovered field */
+  int recovered_order;
+  /* the input field consisting of values of a key
+     quantity at integration points (stress or strain, for example).
+     this field is related to integration_order */
+  Omega_h::Reals eps;
+  /* the recovered field, consisting of nodal values
+     of the quantity of interest.
+     this uses a Lagrange basis of recovered_order */
+  Omega_h::Reals eps_star;
+  /* the acceptable margin of error, expressed as a factor
+     greater than zero.
+     setting this equal to zero would request zero element
+     size everywhere, i.e. infinite refinement.
+     increasing it scales up the desired element size
+     throughout.
+     basically, this is a (nonlinear) scaling factor on the resulting
+     size field */
+  Omega_h::Real tolerance;
+  /* the uniform linear scaling factor derived from the tolerance
+     and integrals of the recovered field over the mesh.
+     desired element size = current size * current error * size_factor */
+  Omega_h::Real size_factor;
+  /* a temporary field storing desired sizes at elements */
+  Omega_h::Reals element_size;
+  /* the resulting size field, recovered from the element_size field
+     (using a local average recovery method much weaker than SPR) */
+  Omega_h::Reals size;
+};
+
+/* useful for initializing values to quickly
+   detect "uninitialized" value bugs */
+static double getNaN()
+{
+  return std::numeric_limits<double>::quiet_NaN();
+}
+
+static void setupEstimation(Mesh& mesh, Estimation* e, const Reals eps, Real tolerance)
+{
+  /* note that getOrder being used to convey this meaning
+     is a bit of a hack, but looks decent if you don't
+     try to define the FieldShape API too rigorously */
+  e->integration_order = apf::getShape(eps)->getOrder(); //FIXME
+  e->mesh = mesh;
+  /* so far recovery order is directly tied to the
+     mesh's coordinate field order, coordinate this
+     with field recovery code */
+  e->recovered_order = e->mesh->getShape()->getOrder(); //FIXME
+  e->eps = eps;
+  e->tolerance = tolerance;
+  e->size_factor = getNaN();
+}
+
+/* computes $\|f\|^2$ */
+class SelfProduct : public SInt
+{
+  public:
+    SelfProduct(Estimation* e):
+      SInt(e->integration_order), estimation(e), element(0)
+    {
+      v.setSize(apf::countComponents(e->eps_star)); //FIXME Vector2?
+    }
+    void atPoint(apf::Vector3 const& p, double w, double dV) //FIXME bulk operation
+    {
+      apf::getComponents(element, p, &v[0]); //FIXME get components of eps_star
+      r += (v * v) * w * dV; //FIXME vector*vector needed
+    }
+  private:
+    Estimation* estimation;
+    apf::Element* element;
+    apf::DynamicVector v;
+};
+
+
+
+
 
 template <typename T>
 void printTagInfo(Omega_h::Mesh& mesh, std::ostringstream& oss, int dim, int tag, std::string type) {
@@ -97,6 +188,10 @@ Reals getEffectiveStrainRate(Mesh& mesh) {
 
 Reals recoverLinearStrain(Mesh& mesh, Reals effectiveStrain) {
   return project_by_fit(&mesh, effectiveStrain);
+}
+
+Reals computeError(Mesh& mesh, Reals effectiveStrain, Reals recoveredStrain) {
+  return error;
 }
 
 int main(int argc, char** argv) {

c8a275d2da64d2c844bc2cc6cfad3e56a7f552f4
Cameron Smith <smithc11@rpi.edu>
2025-03-04 22:47:54 -0500

    executes
    requires omegah cws/generalVtxPatch @ 5e752190

--- test/thwaites_adapt.cpp
+++ test/thwaites_adapt.cpp
@@ -90,14 +90,17 @@ void setupFieldTransfer(AdaptOpts& opts) {
   }
 }
 
-/**
- * retrieve the effective strain rate from the mesh
- *
- * despite the name being 'effective_stress' it is the effective strain:
- * the frobenius norm of  the strain tensor
-  */
-Reals getEffectiveStrainRate(Mesh& mesh) {
-  return mesh.get_array<Real>(2, "effective_stress");
+Reals getMagVelocity(Mesh& mesh) {
+  const auto v_x = mesh.get_array<Real>(VERT, "solution_1");
+  const auto v_y = mesh.get_array<Real>(VERT, "solution_2");
+  Write<Real> magVelocity(mesh.nverts());
+  Kokkos::parallel_for("magVelocity", mesh.nverts(),
+      KOKKOS_LAMBDA(int vtx) {
+         const auto x = v_x[vtx];
+         const auto y = v_y[vtx];
+         magVelocity[vtx] = Kokkos::sqrt(x*x+y*y);
+      });
+  return read(magVelocity);
 }
 
 Reals recoverLinearStrain(Mesh& mesh, Reals effectiveStrain) {
@@ -165,144 +168,61 @@ Reals min_max_normalization_coordinates(const Reals& coordinates, int dim = 2) {
   return read(normalized_coordinates);
 }
 
-Reals recoverStrainPCMS(Mesh& mesh, Reals effectiveStrain, size_t degree) {
+Reals recoverMagVelocityPCMS(Mesh& mesh, Reals magVelocity, size_t degree, size_t minPatchSize) {
   assert(degree > 0 && degree < 4);
+  assert(minPatchSize > 0 && minPatchSize < 20);
+  assert(magVelocity.size() == mesh.nverts());
+  assert(mesh.nverts() > 0);
   const auto dim = mesh.dim();
   const Real tolerance = 5e-4;
 
-  // min_max normalise target coordinates
-  const auto& target_coordinates_original = mesh.coords();
-  auto target_coordinates = min_max_normalization_coordinates(target_coordinates_original, dim);
+  const auto target_coordinates = mesh.coords();
 
-  const auto source_coordinates = getElementCentroids(mesh, target_coordinates);
+  const auto source_coordinates = mesh.coords();
 
-  size_t min_patch_size = 0;
-  if(degree==1)
-    min_patch_size = 3;
-  if(degree==2)
-    min_patch_size = 10; //6 results in a noisy field
-  if(degree==3)
-    min_patch_size = 10;
-  const auto patches = mesh.get_vtx_patches(min_patch_size);
+  const auto patches = mesh.get_vtx_patches(minPatchSize, VERT);
   Omega_h::Write<Real> ignored(patches.ab2b.size(), 1);
   SupportResults support{patches.a2ab,patches.ab2b,ignored};
 
-  const auto source_values = effectiveStrain;
+  const auto source_values = magVelocity;
   auto recovered =
     pcms::mls_interpolation (source_values, source_coordinates, target_coordinates,
         support, dim, degree, support.radii2, pcms::RadialBasisFunction::NO_OP);
 
-  //DEBUG {
-  //print problematic patches
-  const auto recoveredStrainPCMS_h = Omega_h::HostRead<Omega_h::Real>(recovered);
-  const auto bad_points = {4849, 5053};
-  for( auto& pt : bad_points ) {
-    std::cout << std::setprecision (15);
-    std::cout << "recoveredStrainMlsInterp[" << pt << "] " << recoveredStrainPCMS_h[pt] << "\n";
-    print_patch_vals(patches, target_coordinates, source_values, source_coordinates, pt);
-  }
-  //END DEBUG }
-
   return recovered;
 }
-
-template <typename ShapeField>
-void setFieldAtVertices(Omega_h::Mesh &mesh, Reals recoveredStrain, ShapeField field) {
-  const auto MeshDim = mesh.dim();
-  auto setFieldAtVertices = KOKKOS_LAMBDA(const int &vtx) {
-    field(0, 0, vtx, MeshField::Vertex) = recoveredStrain[vtx];
-  };
-  MeshField::parallel_for(ExecutionSpace(), {0}, {mesh.nverts()},
-                          setFieldAtVertices, "setFieldAtVertices");
-}
-
-void printTriCount(Mesh* mesh) {
-  const auto nTri = mesh->nglobal_ents(2);
-  if (!mesh->comm()->rank())
-    std::cout << "nTri: " << nTri << "\n";
-}
-
 
 int main(int argc, char** argv) {
   feenableexcept(FE_ALL_EXCEPT & ~FE_INEXACT);  // Enable all floating point exceptions but FE_INEXACT
   auto lib = Library(&argc, &argv);
-  if( argc != 6 ) {
-    fprintf(stderr, "Usage: %s inputMesh.osh outputMeshPrefix adaptRatio min_size max_size\n", argv[0]);
+  if( argc != 4 ) {
+    fprintf(stderr, "Usage: %s inputMesh.osh outputMeshPrefix minPatchSize\n", argv[0]);
     exit(EXIT_FAILURE);
   }
   auto world = lib.world();
   Omega_h::Mesh mesh(&lib);
   Omega_h::binary::read(argv[1], world, &mesh);
   const auto prefix = std::string(argv[2]);
-  //adaptRatio = 0.1 is used in scorec/core:cws/sprThwaites test/spr_test.cc
-  const MeshField::Real adaptRatio = std::stof(argv[3]);
-  const MeshField::Real min_size = std::stof(argv[4]); //the smallest edge length in initial mesh is 1
-  const MeshField::Real max_size = std::stof(argv[5]); //a value of 8 or 16 roughly maintains the boundary shape
-                                                       //in the downstream parts of the domain
-                                                       //where there is significant coarsening
-  std::cout << "input mesh: " << argv[1] << " outputMeshPrefix: "
-            << prefix << " adaptRatio: " << adaptRatio
-            << " max_size: " << max_size
-            << " min_size: " << min_size << "\n";
-  const auto outname = prefix + "_adaptRatio_" + std::string(argv[3]) +
-                       "_maxSz_" + std::to_string(max_size) +
-                       "_minSz_" + std::to_string(min_size);
+  const auto minPatchSize = std::atoi(argv[3]);
+  std::cout << "input mesh: " << argv[1] << " outputMeshPrefix: " << prefix << " minPatchSize: " << minPatchSize << "\n";
+  const auto outname = prefix + "_recoverMagVelocity_deg2";
 
-  auto effectiveStrain = getEffectiveStrainRate(mesh);
-  auto recoveredStrain = recoverLinearStrain(mesh,effectiveStrain);
+  const auto magVelocity = getMagVelocity(mesh);
+  mesh.add_tag<Real>(VERT, "magVelocity", 1, magVelocity);
+
+  { //write vtk
+  const std::string vtkFileName = "beforeMls" + outname + ".vtk";
+  Omega_h::vtk::write_parallel(vtkFileName, &mesh, 2);
+  }
+
   const auto recoveredFieldDegree = 2;
-  auto recoveredStrainPCMS = recoverStrainPCMS(mesh,effectiveStrain, recoveredFieldDegree);
-  mesh.add_tag<Real>(VERT, "recoveredStrain", 1, recoveredStrain);
-  mesh.add_tag<Real>(VERT, "recoveredStrainPCMS", 1, recoveredStrainPCMS);
-
-  const auto delta_abs = Omega_h::fabs_each(Omega_h::subtract_each(recoveredStrain,recoveredStrainPCMS));
-  const auto max_delta_abs = Omega_h::get_max(delta_abs);
-  std::cout << "mls_interpolation vs project_by_fit max_delta_abs " << max_delta_abs << "\n";
-
-
-  MeshField::OmegahMeshField<ExecutionSpace, MeshField::KokkosController> omf(
-        mesh);
-
-  const auto MeshDim = 2;
-  const auto ShapeOrder = 1;
-  auto recoveredStrainField = omf.CreateLagrangeField<Real, ShapeOrder, MeshDim>();
-  setFieldAtVertices(mesh, recoveredStrainPCMS, recoveredStrainField);
-
-  auto coordField = omf.getCoordField();
-  const auto [shp, map] = MeshField::Omegah::getTriangleElement<ShapeOrder>(mesh);
-  MeshField::FieldElement coordFe(mesh.nelems(), coordField, shp, map);
-
-  auto estimation = Estimation(mesh, effectiveStrain,
-      recoveredStrainField, adaptRatio);
-
-  const auto tgtLength = getSprSizeField(estimation, omf, coordFe);
-  Omega_h::Write<MeshField::Real> tgtLength_oh(tgtLength);
-  mesh.add_tag<Real>(VERT, "tgtLength", 1, tgtLength_oh);
+  auto recoveredMagVelocityPCMS = recoverMagVelocityPCMS(mesh, magVelocity, recoveredFieldDegree, minPatchSize);
+  mesh.add_tag<Real>(VERT, "recoveredMagVelocityPCMS", 1, recoveredMagVelocityPCMS);
 
   { //write vtk
   const std::string vtkFileName = "beforeAdapt" + outname + ".vtk";
   Omega_h::vtk::write_parallel(vtkFileName, &mesh, 2);
-  const std::string vtkFileName_edges = "beforeAdapt" + outname + "_edges.vtk";
-  Omega_h::vtk::write_parallel(vtkFileName_edges, &mesh, 1);
   }
-
-  //adapt
-  auto opts = Omega_h::AdaptOpts(&mesh);
-  setupFieldTransfer(opts);
-  printTriCount(&mesh);
-
-  auto verbose = true;
-  const auto isos = Omega_h::isos_from_lengths(tgtLength_oh);
-  auto metric = clamp_metrics(mesh.nverts(), isos, min_size, max_size);
-  Omega_h::grade_fix_adapt(&mesh, opts, metric, verbose);
-
-  { //write vtk and osh for adapted mesh
-  const std::string outfilename = "afterAdapt" + outname;
-  Omega_h::vtk::write_parallel(outfilename + ".vtk", &mesh, 2);
-  Omega_h::binary::write(outfilename + ".osh", &mesh);
-  std::cout << "wrote adapted mesh: " << outfilename + ".osh" << "\n";
-  }
-
 
   return 0;
 }

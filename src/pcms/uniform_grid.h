5e2d3d8f355f71a853f696d9d51964cfa4886f31
Matthew McCall <m-mccall+git@outlook.com>
2025-03-11 10:18:18 -0400

    Refactor GetCellBBOX for clarity and dimensional flexibility.
    Revised the method to enhance readability and adaptability to arbitrary dimensions. Simplified calculations by introducing loops and standard algorithms, ensuring consistent and maintainable code structure.

--- src/pcms/uniform_grid.h
+++ src/pcms/uniform_grid.h
@@ -45,13 +45,20 @@ public:
 
   [[nodiscard]] KOKKOS_INLINE_FUNCTION AABBox<dim> GetCellBBOX(LO idx) const
   {
-    auto [i, j] = GetDimensionedIndex(idx);
-    std::array<Real, dim> half_width = {edge_length[0] / (2.0 * divisions[0]),
-                                        edge_length[1] / (2.0 * divisions[1])};
-    AABBox<dim> bbox{.center = {(2.0 * j + 1.0) * half_width[0]+bot_left[0],
-                                (2.0 * i + 1.0) * half_width[1]+bot_left[1]},
-                     .half_width = half_width};
-    return bbox;
+    auto index = GetDimensionedIndex(idx);
+    std::reverse(index.begin(), index.end());
+
+    std::array<Real, dim> half_width, center;
+
+    std::transform(edge_length.begin(), edge_length.end(), divisions.begin(), half_width.begin(), [](const Real edge_length, const LO division) {
+      return edge_length / division / 2;
+    });
+
+    for (int i = 0; i < dim; ++i) {
+      center[i] = (2.0 * index[i] + 1.0) * half_width[i] + bot_left[i];
+    }
+
+    return { .center = center, .half_width = half_width };
   }
 
   [[nodiscard]] KOKKOS_INLINE_FUNCTION std::array<LO, dim> GetDimensionedIndex(LO idx) const

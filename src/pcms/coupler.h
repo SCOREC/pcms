6eb73c8d56029d33fb93db8dd90374fba5fb1512
Angelyr <scardking@gmail.com>
2025-03-26 15:51:24 -0400

    created coupler class

--- src/pcms/coupler.h
+++ src/pcms/coupler.h
@@ -1,24 +1,111 @@
-#ifndef PCMS_COUPLING_CLIENT_H
-#define PCMS_COUPLING_CLIENT_H
+#ifndef PCMS_COUPLER_H
+#define PCMS_COUPLER_H
 #include "pcms/common.h"
 #include "pcms/field_communicator.h"
 #include "pcms/profile.h"
-#include "pcms/coupler.h"
 
 namespace pcms
 {
 
-class CouplerClient
+class CoupledField
 {
 public:
-  CouplerClient(std::string name, MPI_Comm comm,
+  template <typename FieldAdapterT>
+  CoupledField(const std::string& name, FieldAdapterT field_adapter,
+               MPI_Comm mpi_comm, redev::Redev& redev, redev::Channel& channel,
+               bool participates)
+  {
+    PCMS_FUNCTION_TIMER;
+    MPI_Comm mpi_comm_subset = MPI_COMM_NULL;
+    PCMS_ALWAYS_ASSERT((mpi_comm == MPI_COMM_NULL) ? (participates == false)
+                                                   : true);
+    if (mpi_comm != MPI_COMM_NULL) {
+      int rank = -1;
+      MPI_Comm_rank(mpi_comm, &rank);
+      MPI_Comm_split(mpi_comm, participates ? 0 : MPI_UNDEFINED, rank,
+                     &mpi_comm_subset);
+    }
+    coupled_field_ =
+      std::make_unique<CoupledFieldModel<FieldAdapterT, FieldAdapterT>>(
+        name, std::move(field_adapter), mpi_comm_subset, redev, channel,
+        participates);
+  }
+
+  void Send(Mode mode = Mode::Synchronous)
+  {
+    PCMS_FUNCTION_TIMER;
+    coupled_field_->Send(mode);
+  }
+  void Receive()
+  {
+    PCMS_FUNCTION_TIMER;
+    coupled_field_->Receive();
+  }
+  struct CoupledFieldConcept
+  {
+    virtual void Send(Mode) = 0;
+    virtual void Receive() = 0;
+    virtual ~CoupledFieldConcept() = default;
+  };
+  template <typename FieldAdapterT, typename CommT>
+  struct CoupledFieldModel final : CoupledFieldConcept
+  {
+    using value_type = typename FieldAdapterT::value_type;
+
+    CoupledFieldModel(const std::string& name, FieldAdapterT&& field_adapter,
+                      MPI_Comm mpi_comm_subset, redev::Redev& redev,
+                      redev::Channel& channel, bool participates)
+      : mpi_comm_subset_(mpi_comm_subset),
+        field_adapter_(std::move(field_adapter)),
+        comm_(FieldCommunicator<CommT>(name, mpi_comm_subset_, redev, channel,
+                                       field_adapter_))
+    {
+      PCMS_FUNCTION_TIMER;
+    }
+    void Send(Mode mode) final
+    {
+      PCMS_FUNCTION_TIMER;
+      comm_.Send(mode);
+    };
+    void Receive() final
+    {
+      PCMS_FUNCTION_TIMER;
+      comm_.Receive();
+    };
+    ~CoupledFieldModel()
+    {
+      PCMS_FUNCTION_TIMER;
+      if (mpi_comm_subset_ != MPI_COMM_NULL)
+        MPI_Comm_free(&mpi_comm_subset_);
+    }
+
+    MPI_Comm mpi_comm_subset_;
+    FieldAdapterT field_adapter_;
+    FieldCommunicator<CommT> comm_;
+  };
+
+private:
+  std::unique_ptr<CoupledFieldConcept> coupled_field_;
+};
+
+class Coupler
+{
+private:
+  redev::Redev SetUpRedev(bool isRdv, redev::Partition partition) {
+    if (isRdv)
+      return redev::Redev(mpi_comm_, std::move(partition));
+    else
+      return redev::Redev(mpi_comm_);
+  }
+public:
+  Coupler(std::string name, MPI_Comm comm, bool isRdv, redev::Partition partition,
                 redev::TransportType transport_type = redev::TransportType::BP4,
                 adios2::Params params = {{"Streaming", "On"},
-                                         {"OpenTimeoutSecs", "400"}},
+                                         {"OpenTimeoutSecs", "60"}},
                 std::string path = "")
     : name_(std::move(name)),
       mpi_comm_(comm),
-      redev_(comm),
+      redev_(SetUpRedev(isRdv, std::move(partition))),
       channel_{redev_.CreateAdiosChannel(name_, std::move(params),
                                          transport_type, std::move(path))}
   {
@@ -110,4 +197,4 @@ private:
 };
 } // namespace pcms
 
-#endif // PCMS_COUPLING_CLIENT_H
+#endif // PCMS_COUPLER_H

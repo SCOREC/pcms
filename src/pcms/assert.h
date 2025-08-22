ad73ebd5f10c840cead49b299f75c41697df0858
Jacob Merson <jacob@jacobmerson.com>
2025-08-22 02:00:09 -0400

    add prefix to macro variables to avoid shadow warnings

--- src/pcms/assert.h
+++ src/pcms/assert.h
@@ -14,33 +14,32 @@
 
 #define PCMS_ALWAYS_ASSERT(...) VA_SELECT(PCMS_ALWAYS_ASSERT, __VA_ARGS__)
 #define PCMS_ALWAYS_ASSERT_1(cond) PCMS_ALWAYS_ASSERT_2(cond, MPI_COMM_WORLD)
-#define PCMS_ALWAYS_ASSERT_2(cond, comm)                                       \
-  do {                                                                         \
-    if (!(cond)) {                                                             \
-      int rank = -1;                                                           \
-      MPI_Comm_rank(comm, &rank);                                              \
-      char omsg[2048];                                                         \
-      snprintf(omsg, 2048, "%s failed at %s + %d on rank %d\n", #cond,         \
-               __FILE__, __LINE__, rank);                                      \
-      pcms::Pcms_Assert_Fail(omsg);                                            \
-    }                                                                          \
+#define PCMS_ALWAYS_ASSERT_2(pcms_macro_cond, pcms_macro_comm)                                                  \
+  do {                                                                                               \
+    if (!(pcms_macro_cond)) {                                                                                   \
+      int pcms_macro_rank = -1;                                                                      \
+      MPI_Comm_rank(pcms_macro_comm, &pcms_macro_rank);                                              \
+      char pcms_macro_omsg[2048];                                                                    \
+      snprintf(pcms_macro_omsg, 2048, "%s failed at %s + %d on rank %d\n", #pcms_macro_cond,                    \
+               __FILE__, __LINE__, pcms_macro_rank);                                                 \
+      pcms::Pcms_Assert_Fail(pcms_macro_omsg);                                                       \
+    }                                                                                                \
   } while (0)
-#define PCMS_ALWAYS_ASSERT_3(cond, comm, msg)                                    \
-  do {                                                                        \
-    if (!(cond)) {                                                            \
-      int rank = -1;                                                          \
-      MPI_Comm_rank(comm, &rank);                                             \
-      char omsg[2048];                                                        \
-      snprintf(omsg, 2048, "%s failed at %s + %d on rank %d: %s\n", #cond,   \
-               __FILE__, __LINE__, rank, msg);                                \
-      pcms::Pcms_Assert_Fail(omsg);                                           \
-    }                                                                         \
+#define PCMS_ALWAYS_ASSERT_3(pcms_macro_cond, pcms_macro_comm, pcms_macro_msg)                                  \
+  do {                                                                                               \
+    if (!(pcms_macro_cond)) {                                                                                   \
+      int pcms_macro_rank = -1;                                                                      \
+      MPI_Comm_rank(pcms_macro_comm, &pcms_macro_rank);                                              \
+      char pcms_macro_omsg[2048];                                                                    \
+      snprintf(pcms_macro_omsg, 2048, "%s failed at %s + %d on rank %d: %s\n", #pcms_macro_cond,                \
+               __FILE__, __LINE__, pcms_macro_rank, pcms_macro_msg);                                 \
+      pcms::Pcms_Assert_Fail(pcms_macro_omsg);                                                       \
+    }                                                                                                \
   } while (0)
 
-namespace pcms
-{
+namespace pcms {
 // from scorec/core/pcu_fail.h
-void Pcms_Assert_Fail(const char* msg) __attribute__((noreturn));
+void Pcms_Assert_Fail(const char *msg) __attribute__((noreturn));
 } // namespace pcms
 
 #endif // PCMS_COUPLING_ASSERT_H

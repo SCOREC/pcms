2214de41c75cb8410cc4a464289955b374b38562
Cameron Smith <smithc11@rpi.edu>
2025-09-16 20:44:23 -0400

    runtime control of engines and params

--- test/test_proxy_coupling.cpp
+++ test/test_proxy_coupling.cpp
@@ -21,14 +21,44 @@ using namespace std::chrono_literals;
 
 static constexpr bool done = true;
 static constexpr int COMM_ROUNDS = 10;
-static const auto transport = redev::TransportType::SST;
+static std::string sstDataTransport;
+static std::string adiosEngine;
 namespace ts = test_support;
+
+auto getAdiosEngine() {
+  if( adiosEngine == "SST" )
+    return redev::TransportType::SST;
+  else if( adiosEngine == "BP4" )
+    return redev::TransportType::BP4;
+  else
+    exit(EXIT_FAILURE);
+}
+
+adios2::Params getAdiosParams(const redev::TransportType engine) {
+  if( engine != redev::TransportType::SST ) {
+    return {{"Streaming", "On"}, {"OpenTimeoutSecs", "60"}};
+  }
+
+  adios2::Params params;
+  if( sstDataTransport == "MPI" )
+    params = {{"DataTransport", "MPI"}, {"Streaming", "On"}, {"OpenTimeoutSecs", "60"}};
+  else if( sstDataTransport == "RDMA" )
+    params = {{"DataTransport", "RDMA"}, {"Streaming", "On"}, {"OpenTimeoutSecs", "60"}};
+  else if( sstDataTransport == "WAN" )
+    params = {{"DataTransport", "WAN"}, {"Streaming", "On"}, {"OpenTimeoutSecs", "60"}};
+  else
+    exit(EXIT_FAILURE);
+  return params;
+}
 
 void xgc_delta_f(MPI_Comm comm, Omega_h::Mesh& mesh)
 {
+  int rank;
+  MPI_Comm_rank(comm, &rank);
   pcms::Coupler coupler("proxy_couple", comm, false, {});
-  pcms::Application* app = coupler.AddApplication("proxy_couple_xgc_delta_f", "", transport);
-
+  const auto adiosEngine = getAdiosEngine();
+  const auto adiosParams = getAdiosParams(adiosEngine);
+  pcms::Application* app = coupler.AddApplication("proxy_couple_xgc_delta_f", "", adiosEngine, adiosParams);
   auto is_overlap = ts::markOverlapMeshEntities(mesh, ts::IsModelEntInOverlap{});
   app->AddField("gids",
                OmegaHFieldAdapter<GO>("global", mesh, is_overlap));
@@ -45,14 +75,19 @@ void xgc_delta_f(MPI_Comm comm, Omega_h::Mesh& mesh)
       app->ReceiveField("gids"); //(Alt) df_gid_field->Receive();
       app->EndReceivePhase();
       // cpl.ReceiveField("gids2"); //(Alt) df_gid_field->Receive();
+      if(!rank) fprintf(stderr, "round %d is done\n", i);
     }
   } while (!done);
   MPI_Barrier(comm);
 }
 void xgc_total_f(MPI_Comm comm, Omega_h::Mesh& mesh)
 {
+  int rank;
+  MPI_Comm_rank(comm, &rank);
   pcms::Coupler coupler("proxy_couple", comm, false, {});
-  pcms::Application* app = coupler.AddApplication("proxy_couple_xgc_total_f", "", transport);
+  const auto adiosEngine = getAdiosEngine();
+  const auto adiosParams = getAdiosParams(adiosEngine);
+  pcms::Application* app = coupler.AddApplication("proxy_couple_xgc_total_f", "", adiosEngine, adiosParams);
   auto is_overlap = ts::markOverlapMeshEntities(mesh, ts::IsModelEntInOverlap{});
   app->AddField("gids",
                OmegaHFieldAdapter<GO>("global", mesh, is_overlap));
@@ -65,12 +100,15 @@ void xgc_total_f(MPI_Comm comm, Omega_h::Mesh& mesh)
       app->BeginReceivePhase();
       app->ReceiveField("gids"); //(Alt) tf_gid_field->Receive();
       app->EndReceivePhase();
+      if(!rank) fprintf(stderr, "round %d is done\n", i);
     }
   } while (!done);
   MPI_Barrier(comm);
 }
 void xgc_coupler(MPI_Comm comm, Omega_h::Mesh& mesh, std::string_view cpn_file)
 {
+  int rank;
+  MPI_Comm_rank(comm, &rank);
   // coupling server using same mesh as application
   // note the xgc_coupler stores a reference to the internal mesh and it is the
   // user responsibility to keep it alive!
@@ -80,8 +118,10 @@ void xgc_coupler(MPI_Comm comm, Omega_h::Mesh& mesh, std::string_view cpn_file)
   const auto partition = std::get<redev::ClassPtn>(cpl.GetPartition());
   auto is_overlap =
     ts::markServerOverlapRegion(mesh, partition, ts::IsModelEntInOverlap{});
-  auto* total_f = cpl.AddApplication("proxy_couple_xgc_total_f", "", transport);
-  auto* delta_f = cpl.AddApplication("proxy_couple_xgc_delta_f", "", transport);
+  const auto adiosEngine = getAdiosEngine();
+  const auto adiosParams = getAdiosParams(adiosEngine);
+  auto* total_f = cpl.AddApplication("proxy_couple_xgc_total_f", "", adiosEngine, adiosParams);
+  auto* delta_f = cpl.AddApplication("proxy_couple_xgc_delta_f", "", adiosEngine, adiosParams);
   // TODO, fields should have a transfer policy rather than parameters
   auto* total_f_gids = total_f->AddField(
     "gids", OmegaHFieldAdapter<GO>("total_f_gids", mesh, is_overlap));
@@ -103,6 +143,7 @@ void xgc_coupler(MPI_Comm comm, Omega_h::Mesh& mesh, std::string_view cpn_file)
         delta_f_gids->Send(pcms::Mode::Deferred);
         delta_f_gids2->Send(pcms::Mode::Deferred);
       });
+      if(!rank) fprintf(stderr, "round %d is done\n", i);
     }
   } while (!done);
   MPI_Barrier(comm);
@@ -112,22 +153,34 @@ void xgc_coupler(MPI_Comm comm, Omega_h::Mesh& mesh, std::string_view cpn_file)
 
 int main(int argc, char** argv)
 {
+  int provide;
+  MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provide);
   auto lib = Omega_h::Library(&argc, &argv);
   auto world = lib.world();
   const int rank = world->rank();
-  if (argc != 4) {
+  if(!rank) std::cerr << "mpi thread level: " << provide << "\n";
+
+  if (argc != 6) {
     if (!rank) {
       std::cerr << "Usage: " << argv[0]
                 << " <clientId=-1|0|1> /path/to/omega_h/mesh "
-                   "/path/to/partitionFile.cpn\n";
+                   "/path/to/partitionFile.cpn "
+                   "sstDataTransport=[RDMA|WAN|MPI] "
+                   "adiosEngine=[BP4|SST]\n";
     }
     exit(EXIT_FAILURE);
   }
-  OMEGA_H_CHECK(argc == 4);
+  OMEGA_H_CHECK(argc == 6);
   const auto clientId = atoi(argv[1]);
   REDEV_ALWAYS_ASSERT(clientId >= -1 && clientId <= 1);
   const auto meshFile = argv[2];
   const auto classPartitionFile = argv[3];
+  sstDataTransport = argv[4];
+  adiosEngine = argv[5];
+  if(!rank) {
+    std::cerr << "inputs: " << clientId << ", " << meshFile << " " << classPartitionFile << " "
+              << sstDataTransport << " " << adiosEngine << "\n";
+  }
   Omega_h::Mesh mesh(&lib);
   Omega_h::binary::read(meshFile, lib.world(), &mesh);
   MPI_Comm mpi_comm = lib.world()->get_impl();

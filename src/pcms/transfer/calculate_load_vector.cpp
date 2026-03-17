54213bb7824bb6d4aa49f3d462b9029f563eb7b4
abhiyanpaudel <abhiyanp93@gmail.com>
2026-03-17 17:22:29 -0400

    add build load vector with mc approach

--- src/pcms/transfer/calculate_load_vector.cpp
+++ src/pcms/transfer/calculate_load_vector.cpp
@@ -21,7 +21,52 @@ PetscErrorCode calculateLoadVector(Omega_h::Mesh& target_mesh,
 
   // Fill COO values
   auto elmLoadVector =
-    buildLoadVector(target_mesh, source_mesh, intersection, source_values);
+    buildLoadVectorMI(target_mesh, source_mesh, intersection, source_values);
+
+  auto hostElmLoadVector = Kokkos::create_mirror_view(elmLoadVector);
+  Kokkos::deep_copy(hostElmLoadVector, elmLoadVector);
+  PetscCheck(static_cast<PetscInt>(hostElmLoadVector.extent(0)) == nnz,
+             PETSC_COMM_SELF, PETSC_ERR_ARG_SIZ,
+             "Element load vector size (%d) does not match COO nnz (%d)",
+             static_cast<PetscInt>(hostElmLoadVector.extent(0)), nnz);
+
+  for (PetscInt e = 0; e < nnz; ++e) {
+    coo_vals[e] = hostElmLoadVector(e);
+  }
+
+  // create vector with preallocated COO structure
+  Vec vec;
+  PetscCall(createSeqVec(PETSC_COMM_WORLD, target_mesh.nverts(), &vec));
+  PetscCall(VecSetPreallocationCOO(vec, nnz, coo_i));
+  PetscCall(VecSetValuesCOO(vec, coo_vals, ADD_VALUES));
+  PetscCall(PetscFree(coo_i));
+  PetscCall(PetscFree(coo_vals));
+
+  if (target_mesh.nelems() < 10) {
+    PetscCall(VecView(vec, PETSC_VIEWER_STDOUT_WORLD));
+  }
+
+  *loadVec_out = vec;
+  PetscFunctionReturn(PETSC_SUCCESS);
+}
+
+PetscErrorCode calculateLoadVectorMC(
+  Omega_h::Mesh& target_mesh, const Omega_h::Reals& field_values_at_points,
+  const int npoints_each_tri, SamplingMethod method, Vec* loadVec_out,
+  const std::string sobol_filename)
+{
+
+  PetscFunctionBeginUser;
+  PetscInt nnz = 0;
+  PetscInt* coo_i = nullptr;
+  PetscCall(build_linear_triangle_coo_rows(target_mesh, &coo_i, &nnz));
+  PetscScalar* coo_vals = nullptr;
+  PetscCall(PetscMalloc1(nnz, &coo_vals));
+
+  // Fill COO values
+  auto elmLoadVector =
+    buildLoadVectorMC(target_mesh, field_values_at_points, npoints_each_tri,
+                      method, sobol_filename);
 
   auto hostElmLoadVector = Kokkos::create_mirror_view(elmLoadVector);
   Kokkos::deep_copy(hostElmLoadVector, elmLoadVector);
